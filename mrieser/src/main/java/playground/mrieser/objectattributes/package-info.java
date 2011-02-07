5b63e5da4e6125312ddeca45a91e51734b821ecd
mrieser <rieser@gmail.com>
2011-02-07 10:58:58 +0000

    moving objectattributes from my playground to org.matsim.utils. including tests and new dtd.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@14189 a0517364-e873-4167-b1c8-416daa5a6b91

--- mrieser/src/main/java/playground/mrieser/objectattributes/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Supports keeping track of arbitrary attributes for identifiable objects.
- *
- * An example usage can be seen in {@link Example}.
- */
-package playground.mrieser.objectattributes;
