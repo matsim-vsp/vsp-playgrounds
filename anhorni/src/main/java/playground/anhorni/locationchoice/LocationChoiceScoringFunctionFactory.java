0b8248129e6554e613b019550a6bec855458ef0b
michaz <michael.zilske@gmail.com>
2011-09-15 18:07:20 +0000

    Reworked the internals of the scoring so that it doesn't need the plan anymore.
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@16656 a0517364-e873-4167-b1c8-416daa5a6b91

--- anhorni/src/main/java/playground/anhorni/locationchoice/LocationChoiceScoringFunctionFactory.java
+++ anhorni/src/main/java/playground/anhorni/locationchoice/LocationChoiceScoringFunctionFactory.java
@@ -1,10 +1,10 @@
 /* *********************************************************************** *
  * project: org.matsim.*
- * CharyparNagelScoringFunctionFactoryWithDetailedLegScoreRecord.java
+ * LocationChoiceScoringFunctionFactory.java
  *                                                                         *
  * *********************************************************************** *
  *                                                                         *
- * copyright       : (C) 2010 by the members listed in the COPYING,        *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
  *                   LICENSE and WARRANTY file.                            *
  * email           : info at matsim dot org                                *
  *                                                                         *
@@ -18,39 +18,52 @@
  *                                                                         *
  * *********************************************************************** */
 
-package playground.yu.scoring.postProcessing;
+package playground.anhorni.locationchoice;
 
+import java.util.TreeMap;
+
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.api.experimental.facilities.ActivityFacilities;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.scoring.CharyparNagelScoringParameters;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.ScoringFunctionAccumulator;
-import org.matsim.core.scoring.charyparNagel.ActivityScoringFunction;
+import org.matsim.core.scoring.ScoringFunctionFactory;
 import org.matsim.core.scoring.charyparNagel.AgentStuckScoringFunction;
-import org.matsim.core.scoring.charyparNagel.CharyparNagelScoringFunctionFactory;
+import org.matsim.core.scoring.charyparNagel.LegScoringFunction;
 import org.matsim.core.scoring.charyparNagel.MoneyScoringFunction;
+import org.matsim.locationchoice.facilityload.FacilityPenalty;
 
-public class CharyparNagelScoringFunctionFactoryWithDetailedLegScoreRecord
-		extends CharyparNagelScoringFunctionFactory {
 
-	public CharyparNagelScoringFunctionFactoryWithDetailedLegScoreRecord(
-			PlanCalcScoreConfigGroup config) {
-		super(config);
-		// TODO Auto-generated constructor stub
+/**
+ * A factory to create {@link LocationChoiceScoringFunction}s.
+ *
+ * @author anhorni
+ */
+public class LocationChoiceScoringFunctionFactory implements ScoringFunctionFactory {
+
+	private final TreeMap<Id, FacilityPenalty> facilityPenalties;
+
+	private final CharyparNagelScoringParameters params;
+	private final ActivityFacilities facilities;
+	
+	public LocationChoiceScoringFunctionFactory(final PlanCalcScoreConfigGroup config, 
+			final TreeMap<Id, FacilityPenalty> facilityPenalties, final ActivityFacilities facilities) {
+		this.params = new CharyparNagelScoringParameters(config);
+		this.facilityPenalties = facilityPenalties;
+		this.facilities = facilities;
 	}
-
-	public ScoringFunction createNewScoringFunction(Plan plan) {
+	
+	@Override
+	public ScoringFunction createNewScoringFunction(final Plan plan) {
+		
 		ScoringFunctionAccumulator scoringFunctionAccumulator = new ScoringFunctionAccumulator();
-		scoringFunctionAccumulator
-				.addScoringFunction(new ActivityScoringFunction(getParams()));
-
-		LegScoringFunctionWithDetailedRecord legScoring = new LegScoringFunctionWithDetailedRecord(
-				plan, getParams());
-		scoringFunctionAccumulator.addScoringFunction(legScoring);
-
-		scoringFunctionAccumulator.addScoringFunction(new MoneyScoringFunction(
-				getParams()));
-		scoringFunctionAccumulator
-				.addScoringFunction(new AgentStuckScoringFunction(getParams()));
+		scoringFunctionAccumulator.addScoringFunction(new LocationChoiceScoringFunction(plan, params, facilityPenalties, this.facilities));
+		scoringFunctionAccumulator.addScoringFunction(new LegScoringFunction(params));
+		scoringFunctionAccumulator.addScoringFunction(new MoneyScoringFunction(params));
+		scoringFunctionAccumulator.addScoringFunction(new AgentStuckScoringFunction(params));
 		return scoringFunctionAccumulator;
 	}
+
 }
