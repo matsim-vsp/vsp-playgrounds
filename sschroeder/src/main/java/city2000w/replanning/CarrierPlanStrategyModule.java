0e58afe9dfe7e8aff2ea5f632718b6120c0eb0dd
michaz <michael.zilske@gmail.com>
2011-11-14 19:11:01 +0000

    freight
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@17335 a0517364-e873-4167-b1c8-416daa5a6b91

--- sschroeder/src/main/java/city2000w/replanning/CarrierPlanStrategyModule.java
+++
@@ -1,9 +0,0 @@
-package city2000w.replanning;
-
-import org.matsim.contrib.freight.carrier.Carrier;
-
-public interface CarrierPlanStrategyModule {
-	
-	public void handleActor(Carrier carrier);
-
-}
--- sschroeder/src/main/java/city2000w/replanning/PlanStrategy.java
+++
@@ -1,8 +0,0 @@
-package city2000w.replanning;
-
-import org.matsim.contrib.freight.carrier.Actor;
-
-public interface PlanStrategy<T extends Actor> {
-	
-	public void run(T agent);
-}
