e6255c90cae92fefaf6b96bdc5be69ed9c6d8799
michal-m <michal.mac@gmail.com>
2013-12-19 13:59:58 +0000

    refactoring
    git-svn-id: file:///srv/local/data/cleanRepo/trunk@27272 a0517364-e873-4167-b1c8-416daa5a6b91

---
+++ michalm/src/main/java/playground/michalm/taxi/optimizer/immediaterequest/VehicleFinder.java
@@ -0,0 +1,29 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2013 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.michalm.taxi.optimizer.immediaterequest;
+
+import pl.poznan.put.vrp.dynamic.data.model.Vehicle;
+import playground.michalm.taxi.model.TaxiRequest;
+
+
+public interface VehicleFinder
+{
+    public Vehicle findVehicle(TaxiRequest req);
+}
