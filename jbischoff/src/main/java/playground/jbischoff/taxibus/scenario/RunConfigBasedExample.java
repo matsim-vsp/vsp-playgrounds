d59f719eefd91d9db53a872470ad8863d061c76c
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-02-07 13:11:02 +0100

    cleanup in taxibus, preparing move taxibus to taxi contrib

--- jbischoff/src/main/java/playground/jbischoff/taxibus/scenario/RunConfigBasedExample.java
+++ jbischoff/src/main/java/playground/jbischoff/taxibus/scenario/RunConfigBasedExample.java
@@ -17,7 +17,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.jbischoff.taxibus.run;
+package playground.jbischoff.taxibus.scenario;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
