665944d387df83714b68d71d642b1a4dae2e7fbd
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-07-07 22:50:23 +0200

    submitting scripts and other commands on cluster

--- agarwalamit/src/main/java/playground/agarwalamit/templates/RunSSHScript.java
+++ agarwalamit/src/main/java/playground/agarwalamit/templates/RunSSHScript.java
@@ -19,29 +19,40 @@
 
 package playground.agarwalamit.templates;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import playground.sebhoerl.mexec.ssh.utils.SSHFile;
+import playground.sebhoerl.mexec.ssh.utils.SSHUtils;
 
 /**
  * Created by amit on 28/03/2017.
  */
 
-
 public class RunSSHScript {
 
-    private static final String myPassword = "xxx";
+    private static final String myPassword = "xxxx";
 
     public static void main(String[] args) {
+        new RunSSHScript().runViaChannelDirectly();
+    }
+
+    /*
+     * dependency only on Jsch lib.
+     */
+    public static void runViaChannelDirectly() {
 
         try {
             JSch jSch = new JSch();
             jSch.setKnownHosts("~/.ssh/known_hosts"); // location of the ssh fingerprint (unique host key)
 
             Properties config = new Properties();
-
             config.put("StrictHostKeyChecking", "no"); // so that no question asked, and script run without any problem
 
             Session session = jSch.getSession("agarwal", "cluster-i.math.tu-berlin.de", 22);
@@ -52,24 +63,69 @@ public class RunSSHScript {
                 session.connect();
 
                 ChannelExec channel = (ChannelExec) session.openChannel("exec");
+
                 channel.setCommand("ls -l");
 
+                String script = "/net/ils4/agarwal/patnaOpdyts/script_allModes.sh";
+//                channel.setCommand("qsub "+script);
+
+                String myJobs = "qstat -u agarwal";
+                channel.setCommand(myJobs);
+
+                StringBuilder output = new StringBuilder();
+                InputStream stdout = channel.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stdout));
+
                 channel.connect();
-
-                while (channel.isConnected()) {
-                    System.out.println("connected.");
+                String line = bufferedReader.readLine();
+                while ( line != null) {
+                    output.append(line+"\n");
+                    line = bufferedReader.readLine();
                 }
-                channel.disconnect();
 
+                System.out.println(output.toString());
+                channel.disconnect();
             } finally {
                 session.disconnect();
             }
-
-
         } catch (JSchException e) {
             throw new RuntimeException("Aborting. Reason : " + e);
+        } catch (IOException exx) {
+            throw new RuntimeException("Data is not written/read. Reason : " + exx);
         }
-
     }
 
+    public static void runViaSSHUtils() {
+        try {
+            JSch jSch = new JSch();
+            jSch.setKnownHosts("~/.ssh/known_hosts"); // location of the ssh fingerprint (unique host key)
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no"); // so that no question asked, and script run without any problem
+
+            Session session = jSch.getSession("agarwal", "cluster-i.math.tu-berlin.de", 22);
+            session.setConfig(config);
+            session.setPassword(myPassword);
+
+            try {
+                session.connect();
+
+                SSHUtils sshUtils = new SSHUtils(session);
+                String script = "/net/ils4/agarwal/patnaOpdyts/script_allModes.sh";
+
+                sshUtils.mkdirs(new SSHFile("/net/ils4/agarwal/testDir/"));
+
+                SSHUtils.RunResult runResult = sshUtils.execute("qstat -u agarwal");
+                System.out.println(runResult.output); // TODO idk, how to see the output on the console.
+
+                sshUtils.execute("qsub "+script);
+            } finally {
+                session.disconnect();
+            }
+        } catch (JSchException e) {
+            throw new RuntimeException("Aborting. Reason : " + e);
+        } catch (IOException exx) {
+            throw new RuntimeException("Data is not written/read. Reason : " + exx);
+        }
+    }
 }
