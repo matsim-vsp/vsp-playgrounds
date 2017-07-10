397f511fba47b858f1c3c925758f7e96f33a695d
jfbischoff <bischoff@vsp.tu-berlin.de>
2017-07-10 16:43:32 +0200

    steps towards zonal based drt system

---
+++ jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/saturdaynight/TaxiZoneManager.java
@@ -0,0 +1,204 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package playground.jbischoff.sharedTaxiBerlin.saturdaynight;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.mutable.MutableDouble;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.util.PartialSort;
+import org.matsim.contrib.zone.Zone;
+import org.matsim.core.controler.MatsimServices;
+import org.matsim.core.controler.events.IterationEndsEvent;
+import org.matsim.core.controler.listener.IterationEndsListener;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.core.utils.gis.PolygonFeatureFactory;
+import org.matsim.core.utils.gis.ShapeFileWriter;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.matrices.Matrix;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+
+import playground.jbischoff.utils.JbUtils;
+
+/**
+ * @author  jbischoff
+ *
+ */
+/**
+ *
+ */
+public class TaxiZoneManager implements IterationEndsListener {
+
+	@Inject
+	ZonalOccupancyAggregator aggregator;
+	
+	@Inject 
+	SharedTaxiFareCalculator fareCalculator;
+	
+	@Inject
+	ZonalBasedRequestValidator validator;
+	
+	@Inject
+	MatsimServices matsimServices;
+	private final DecimalFormat format = new DecimalFormat();
+	private boolean headerWritten = false;
+	private double cost_km = 0.3;
+	private double fix_cost_vehicle = 150;
+
+	/**
+	 * 
+	 */
+	public TaxiZoneManager() {
+		format.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+		format.setMinimumIntegerDigits(1);
+		format.setMaximumFractionDigits(3);
+		format.setGroupingUsed(false);	}
+	
+	
+
+	@Override
+	public void notifyIterationEnds(IterationEndsEvent event) {
+		Map<String, Double> zoneOccupancy = aggregator.calculateZoneOccupancy();
+		Map<String, Double> zoneFares = calculateZoneFares(fareCalculator.getFaresPerLink()); 
+		List<String> zonetable = new ArrayList<>();
+		
+        PartialSort<String> worstZoneSort = new PartialSort<>(10);
+        
+		for (Entry<String, Double> e : zoneFares.entrySet()){
+			Double occupancy = zoneOccupancy.get(e.getKey());
+			if (occupancy == null) throw new RuntimeException();
+			double performance = e.getValue()*occupancy;
+			worstZoneSort.add(e.getKey(), performance);
+			zonetable.add(e.getKey()+";"+format.format(occupancy)+";"+format.format(e.getValue())+";"+format.format(performance));
+			
+		}	
+		
+		JbUtils.collection2Text(zonetable,matsimServices.getControlerIO().getIterationPath(event.getIteration())+"zoneperformance.csv", "zone;occupancy;fares;performance");
+		
+		Map<String,Geometry> currentZones = validator.getZones();
+		
+		for(String z : worstZoneSort.retriveKSmallestElements()){
+			currentZones.remove(z);
+		}
+		
+		writeRevenues(event.getIteration(),zoneFares);
+		
+		writeShape(matsimServices.getControlerIO().getIterationPath(event.getIteration())+"zones.shp", currentZones, zoneOccupancy, zoneFares);
+	}
+
+
+
+	/**
+	 * @param zoneFares
+	 */
+	private void writeRevenues(int iteration, Map<String, Double> zoneFares) {
+		BufferedWriter bw = IOUtils.getAppendingBufferedWriter(matsimServices.getControlerIO().getOutputFilename("drt_revenues.txt"));
+		int vkm = (int) (aggregator.getOverallMileage()/1000);
+		int pkm = (int) (aggregator.getRevenueMileage()/1000);
+		double revenue = 0;
+		for (Double f : zoneFares.values()){
+			revenue+=f;
+		}
+		double cost = fix_cost_vehicle * aggregator.getFleetSize() + vkm* cost_km;
+		double profit = revenue-cost;
+		
+			try {
+				if (!headerWritten){
+				bw.write("iteration);vkm;pkm;revenue;cost;profit");
+				headerWritten = true;
+				}
+				bw.newLine();
+				bw.write(iteration+";"+vkm+";"+pkm+";"+revenue+";"+cost+";"+profit);
+				bw.flush();
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+
+
+
+	/**
+	 * @param faresPerLink
+	 * @return
+	 */
+	private Map<String, Double> calculateZoneFares(Map<Id<Link>, MutableDouble> faresPerLink) {
+		Map<String, Double> zoneFares = new HashMap<>();
+		for (Entry<Id<Link>, MutableDouble> e : faresPerLink.entrySet()){
+			String zone = aggregator.getZoneForLinkId(e.getKey());
+			if (zone!=null){
+				if (zoneFares.containsKey(zone)){
+					double fare = zoneFares.get(zone)+e.getValue().getValue();
+					zoneFares.put(zone, fare);
+				} else zoneFares.put(zone, e.getValue().getValue());
+			}
+		}
+		return zoneFares;
+	}
+	
+	
+	  private void writeShape(String outfile,Map<String,Geometry> currentZones, Map<String, Double> zoneOccupancy, Map<String, Double> zoneFares )
+	    {
+		  
+		  	CoordinateReferenceSystem crs = MGC.getCRS(TransformationFactory.DHDN_GK4);
+
+			PolygonFeatureFactory factory = new PolygonFeatureFactory.Builder().addAttribute("ID", String.class)
+					.setCrs(crs).setName("zone").addAttribute("occupancy", Double.class).addAttribute("fare", Double.class).addAttribute("performance", Double.class).create();
+
+			List<SimpleFeature> features = new ArrayList<>();
+			
+			
+			for (Entry<String,Geometry> z :currentZones.entrySet()) {
+                Object[] attribs = new Object[4];
+                attribs[0] = z.getKey();
+                attribs[1] = zoneOccupancy.get(z.getKey());
+                attribs[2] = zoneFares.get(z.getKey());
+                attribs[3] = (Double) attribs[1] * (Double) attribs[2];
+				features.add(factory.createPolygon(z.getValue().getCoordinates(), attribs, z.getKey()));
+			}
+
+			ShapeFileWriter.writeGeometries(features, outfile);
+	    }
+
+}
---
+++ jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/saturdaynight/ZonalOccupancyAggregator.java
@@ -0,0 +1,238 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.jbischoff.sharedTaxiBerlin.saturdaynight;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableDouble;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.ActivityEndEvent;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
+import org.matsim.api.core.v01.events.PersonLeavesVehicleEvent;
+import org.matsim.api.core.v01.events.handler.ActivityEndEventHandler;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonLeavesVehicleEventHandler;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.dvrp.vrpagent.VrpAgentLogic;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.utils.collections.Tuple;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.vehicles.Vehicle;
+
+import com.google.inject.name.Named;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+/**
+ * @author  jbischoff
+ *
+ */
+/**
+ *
+ */
+public class ZonalOccupancyAggregator implements PersonEntersVehicleEventHandler,
+		PersonLeavesVehicleEventHandler, ActivityEndEventHandler,  LinkEnterEventHandler {
+
+	private Map<Id<Vehicle>, MutableInt> currentOccupancy = new HashMap<>();
+	private Set<Id<Person>> taxiDrivers = new HashSet<>();
+	private Map<Id<Link>, DescriptiveStatistics> linkOccupancy = new HashMap<>();
+	private Map<String,Geometry> zones;
+	private Map<Id<Link>,String> link2zone = new HashMap<>();
+	private Map<String,Tuple<MutableDouble,MutableDouble>> zoneOcc = new HashMap<>(); 
+	private Network network;
+	private double overallMileage = 0.0;
+	private double revenueMileage = 0.0;
+	
+	@Inject
+	public ZonalOccupancyAggregator(Network network, EventsManager events, @Named(DrtConfigGroup.DRT_MODE)Map<String,Geometry> drtzones) {
+		this.zones = drtzones;
+		this.network = network;
+		events.addHandler(this);
+		prepareZones();
+	}
+
+	/**
+	 * 
+	 */
+	private void prepareZones() {
+		for (String zoneId : zones.keySet()){
+			zoneOcc.put(zoneId,new Tuple<MutableDouble,MutableDouble>(new MutableDouble(), new MutableDouble()));
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.matsim.core.events.handler.<#reset(int)
+	 */
+	@Override
+	public void reset(int iteration) {
+		taxiDrivers.clear();
+		currentOccupancy.clear();
+		zoneOcc.clear();
+		overallMileage = 0.0;
+		revenueMileage = 0.0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.matsim.api.core.v01.events.handler.ActivityEndEventHandler#
+	 * handleEvent(org.matsim.api.core.v01.events. ActivityEndEvent)
+	 */
+	@Override
+	public void handleEvent(ActivityEndEvent event) {
+		if (event.getActType().equals(VrpAgentLogic.BEFORE_SCHEDULE_ACTIVITY_TYPE)) {
+			Id<Vehicle> vid = Id.createVehicleId(event.getPersonId().toString());
+			currentOccupancy.put(vid, new MutableInt(0));
+			taxiDrivers.add(event.getPersonId());
+		}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.matsim.api.core.v01.events.handler.PersonLeavesVehicleEventHandler#
+	 * handleEvent(org.matsim.api.core.v01.events .PersonLeavesVehicleEvent)
+	 */
+	@Override
+	public void handleEvent(PersonLeavesVehicleEvent event) {
+		if (!this.taxiDrivers.contains(event.getPersonId())){
+		if (this.currentOccupancy.containsKey(event.getVehicleId())) {
+			currentOccupancy.get(event.getVehicleId()).decrement();
+		}}
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler#
+	 * handleEvent(org.matsim.api.core.v01.events .PersonEntersVehicleEvent)
+	 */
+	@Override
+	public void handleEvent(PersonEntersVehicleEvent event) {
+		if (this.currentOccupancy.containsKey(event.getVehicleId())) {
+			if (!this.taxiDrivers.contains(event.getPersonId())) {
+				currentOccupancy.get(event.getVehicleId()).increment();
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.matsim.api.core.v01.events.handler.LinkEnterEventHandler#handleEvent(
+	 * org.matsim.api.core.v01.events.LinkEnterEvent)
+	 */
+	@Override
+	public void handleEvent(LinkEnterEvent event) {
+		if (this.currentOccupancy.containsKey(event.getVehicleId())) {
+			int co = currentOccupancy.get(event.getVehicleId()).intValue();
+			String zone = getZoneForLinkId(event.getLinkId());
+			double linkLength = network.getLinks().get(event.getLinkId()).getLength();
+			if (zone!=null){
+				zoneOcc.get(zone).getFirst().add(co*linkLength);
+				zoneOcc.get(zone).getSecond().add(linkLength);
+			}
+			
+			DescriptiveStatistics linkStats;
+			if (linkOccupancy.containsKey(event.getLinkId())) {
+				linkStats = linkOccupancy.get(event.getLinkId());
+			} else {
+				linkStats = new DescriptiveStatistics();
+			}
+			linkStats.addValue(co);
+			linkOccupancy.put(event.getLinkId(), linkStats);
+			overallMileage += linkLength;
+			revenueMileage += linkLength*co;
+			
+		}
+	}
+
+	/**
+	 * @return the linkOccupancy
+	 */
+	public Map<Id<Link>, DescriptiveStatistics> getLinkOccupancy() {
+		return linkOccupancy;
+	}
+	
+	public String getZoneForLinkId(Id<Link> linkId){
+		if (this.link2zone.containsKey(linkId)){
+			return link2zone.get(linkId);
+		}
+		
+		Point linkCoord = MGC.coord2Point(network.getLinks().get(linkId).getCoord());
+		
+		for (Entry<String, Geometry> e : zones.entrySet()){
+			if (e.getValue().contains(linkCoord)){
+				link2zone.put(linkId, e.getKey());
+				return e.getKey();
+			}
+		}
+		link2zone.put(linkId, null);
+		return null;
+		
+	}
+	public Map<String,Double> calculateZoneOccupancy(){
+		Map<String,Double> zoneOc = new HashMap<>();
+		for (Entry<String,Tuple<MutableDouble,MutableDouble>> e : zoneOcc.entrySet() ){
+			double occ = e.getValue().getFirst().doubleValue()/e.getValue().getSecond().doubleValue();
+			zoneOc.put(e.getKey(),occ );
+		}
+	return zoneOc;
+	}
+	
+	/**
+	 * @return the overallMileage
+	 */
+	public double getOverallMileage() {
+		return overallMileage;
+	}
+	
+	/**
+	 * @return the revenueMileage
+	 */
+	public double getRevenueMileage() {
+		return revenueMileage;
+	}
+	
+	public int getFleetSize(){
+		return currentOccupancy.size();
+	}
+
+}
