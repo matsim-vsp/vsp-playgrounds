ac20444a4a804e09ea5932c717b48fd41a3ca89e
leocamus <leocamusc@gmail.com>
2017-07-17 17:00:37 -0400

    Nothing new, checking if everything is correct

--- santiago/src/main/java/playground/santiago/analysis/eventHandlers/SantiagoModeTripTravelTimeHandler.java
+++ santiago/src/main/java/playground/santiago/analysis/eventHandlers/SantiagoModeTripTravelTimeHandler.java
@@ -21,39 +21,50 @@ import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
 import org.matsim.api.core.v01.events.handler.TransitDriverStartsEventHandler;
 import org.matsim.api.core.v01.population.Person;
-import org.matsim.core.gbl.Gbl;
 import org.matsim.pt.PtConstants;
 
 
-public class SantiagoModalTripTravelTimeHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler,
-		PersonStuckEventHandler, TransitDriverStartsEventHandler, ActivityStartEventHandler {
-	
-	private static final Logger LOGGER = Logger.getLogger(SantiagoModalTripTravelTimeHandler.class);
-	private static final int MAX_STUCK_AND_ABORT_WARNINGS = 15;
-	private final SortedMap<String, Map<Id<Person>, List<String>>> mode2PersonId2TravelTimes;
-	private final Map<Id<Person>, Double> personId2DepartureTime;
-	private int warnCount = 0;
-	private final List<Id<Person>> transitDriverPersons = new ArrayList<>();
-	// agents who first departs with transitWalk and their subsequent modes are stored here until it starts a regular act (home/work/leis/shop)
-	private final Map<Id<Person>, List<String>> modesForTransitUsers = new HashMap<>();
-	private final Map<Id<Person>, Double> transitUserDepartureTime = new HashMap<>();
-	private final Map<Id<Person>, Double> transitUserArrivalTime = new HashMap<>(); 
+/**
+ * Handler to write the departures and travel times of each trip of every person by leg mode. 
+ * It excludes the transitDriverPersons. Transit users are treated separately, see followings
+ * transit_walk - transit_walk --> walk &
+ * transit_walk - pt - transit_walk --> pt &
+ * transit_walk - pt - transit_walk - pt - transit_walk --> pt 
+ */
 
-	public SantiagoModalTripTravelTimeHandler() {
-		this.mode2PersonId2TravelTimes = new TreeMap<>();
-		this.personId2DepartureTime = new HashMap<>();
+public class SantiagoModeTripTravelTimeHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler,
+PersonStuckEventHandler, TransitDriverStartsEventHandler, ActivityStartEventHandler {
+
+	private static final Logger LOGGER = Logger.getLogger(SantiagoModeTripTravelTimeHandler.class);
+	private final SortedMap<String, Map<Id<Person>, List<String>>> modePersonIdDepartureTravelTimes;
+	private final Map<Id<Person>, Double> personIdDepartureTime;
+	private int warnCount = 0;
+
+	private final List<Id<Person>> transitDriverPersons = new ArrayList<>();
+
+
+	private final Map<Id<Person>, Double> transitUserDepartureTime = new HashMap<>();
+	private final Map<Id<Person>, Double> transitUserArrivalTime = new HashMap<>();	
+	private final Map<Id<Person>, List<String>> modesForTransitUsers = new HashMap<>();
+
+
+
+	public SantiagoModeTripTravelTimeHandler() {
+		this.modePersonIdDepartureTravelTimes = new TreeMap<>();
+		this.personIdDepartureTime = new HashMap<>();
 		LOGGER.warn("Excluding the departure and arrivals of transit drivers.");
 	}
 
 	@Override
 	public void reset(int iteration) {
-		this.mode2PersonId2TravelTimes.clear();
-		this.personId2DepartureTime.clear();
+		this.modePersonIdDepartureTravelTimes.clear();
+		this.personIdDepartureTime.clear();
 		this.modesForTransitUsers.clear();
 		this.transitDriverPersons.clear();
 		this.modesForTransitUsers.clear();
 	}
 
+	/*ARRIVAL EVENT*/	
 	@Override
 	public void handleEvent(PersonArrivalEvent event) {
 		Id<Person> personId = event.getPersonId();
@@ -61,30 +72,31 @@ public class SantiagoModalTripTravelTimeHandler implements PersonDepartureEventH
 		double arrivalTime = event.getTime();
 
 		if(transitDriverPersons.remove(personId)) {
-			// exclude arrivals of transit drivers ;
-		}
-		else {
+
+		} else {
 			if( legMode.equals(TransportMode.transit_walk) || legMode.equals(TransportMode.pt) ){
-				transitUserArrivalTime.put(personId, event.getTime()); // store the arrival time
-			} else { // rest of the modes
-				double travelTime = arrivalTime - this.personId2DepartureTime.get(personId);
-				storeData(personId, legMode, this.personId2DepartureTime.get(personId),travelTime);
-				this.personId2DepartureTime.remove(personId);
+				transitUserArrivalTime.put(personId, event.getTime());
+			} else {		
+				double departureTime = this.personIdDepartureTime.get(personId);
+				double travelTime = arrivalTime - departureTime;
+				storeData(personId, legMode, departureTime, travelTime);
+				this.personIdDepartureTime.remove(personId);				
 			}
 		}
 	}
 
+	/*DEPARTURE EVENT*/
 	@Override
 	public void handleEvent(PersonDepartureEvent event) {
 		Id<Person> personId = event.getPersonId();
-		double deartureTime = event.getTime();
+		double departureTime = event.getTime();
 		String legMode = event.getLegMode();
 
 		if(transitDriverPersons.contains(personId)) {
-			// exclude departures of transit drivers and remove them from arrivals
+
 		} else {
-			if (legMode.equals(TransportMode.transit_walk) || legMode.equals(TransportMode.pt) ) { 
-				// transit_walk - transit_walk || transit_walk - pt
+			if (legMode.equals(TransportMode.transit_walk) || legMode.equals(TransportMode.pt) ) {
+
 				if(modesForTransitUsers.containsKey(personId)) {
 					List<String> modes = modesForTransitUsers.get(personId);
 					modes.add(legMode);
@@ -92,10 +104,10 @@ public class SantiagoModalTripTravelTimeHandler implements PersonDepartureEventH
 					List<String> modes = new ArrayList<>();
 					modes.add(legMode);
 					modesForTransitUsers.put(personId, modes);
-					transitUserDepartureTime.put(personId, event.getTime()); // store the departure time, when agent is left after a regular activity
+					transitUserDepartureTime.put(personId, event.getTime());
 				}
 			} else {
-				this.personId2DepartureTime.put(personId, deartureTime);
+				this.personIdDepartureTime.put(personId, departureTime);
 			}
 		} 
 	}
@@ -103,33 +115,33 @@ public class SantiagoModalTripTravelTimeHandler implements PersonDepartureEventH
 	/**
 	 * @return  trip time for each trip of each person segregated w.r.t. travel modes.
 	 */
-	public SortedMap<String, Map<Id<Person>, List<String>>> getLegMode2PesonId2TripTimes (){
-		return this.mode2PersonId2TravelTimes;
+	public SortedMap<String, Map<Id<Person>, List<String>>> getLegModePesonIdTripDepartureTravelTimes (){
+		return this.modePersonIdDepartureTravelTimes;
 	}
 
 	@Override
 	public void handleEvent(PersonStuckEvent event) {
 		this.warnCount++;
-		if(this.warnCount <= MAX_STUCK_AND_ABORT_WARNINGS){
-			LOGGER.warn("'StuckAndAbort' event is thrown for person "+event.getPersonId()+" on link "+event.getLinkId()+" at time "+event.getTime()+
-					". \n Correctness of travel time for such persons can not be guaranteed.");
-			if(this.warnCount== MAX_STUCK_AND_ABORT_WARNINGS) LOGGER.warn(Gbl.FUTURE_SUPPRESSED);
-		}
+		LOGGER.warn("'StuckAndAbort' event is thrown for person "+event.getPersonId()+" on link "+event.getLinkId()+" at time "+event.getTime()+
+				". \n Correctness of travel time for such persons can not be guaranteed. The number of stuckAndAbort events are " + warnCount);
 	}
+
 	@Override
 	public void handleEvent(TransitDriverStartsEvent event) {
 		transitDriverPersons.add(event.getDriverId());
 	}
 
+	/*ACTIVITY START EVENT*/
 	@Override
 	public void handleEvent(ActivityStartEvent event) {
-		if( modesForTransitUsers.containsKey(event.getPersonId()) ) {
-			if(! event.getActType().equals(PtConstants.TRANSIT_ACTIVITY_TYPE) ) { 
-				List<String> modes = modesForTransitUsers.remove(event.getPersonId());
+		Id<Person> personId = event.getPersonId();
+		if( modesForTransitUsers.containsKey(personId) ) {
+			if(! event.getActType().equals(PtConstants.TRANSIT_ACTIVITY_TYPE) ) { //Si la persona llega a una actividad regular.
+				List<String> modes = modesForTransitUsers.remove(event.getPersonId()); //Se borra de la lista de modos acumulados previo a una actividad regular
 				String legMode = modes.contains(TransportMode.pt) ? TransportMode.pt : TransportMode.walk;
 				double departureTime = transitUserDepartureTime.remove(event.getPersonId());
 				double arrivalTime = transitUserArrivalTime.remove(event.getPersonId());
-				storeData(event.getPersonId(), legMode, departureTime,arrivalTime - departureTime);
+				storeData(personId, legMode, departureTime, arrivalTime - departureTime);
 			} else { 
 				// else continue
 			}
@@ -138,25 +150,25 @@ public class SantiagoModalTripTravelTimeHandler implements PersonDepartureEventH
 		}
 	}
 
-	private void storeData(final Id<Person> personId, final String legMode, final double startTime, final double travelTime){
-		if(this.mode2PersonId2TravelTimes.containsKey(legMode)){
-			Map<Id<Person>, List<String>> personId2TravelTimes = this.mode2PersonId2TravelTimes.get(legMode);
-			if(personId2TravelTimes.containsKey(personId)){
-				List<String> travelTimes = personId2TravelTimes.get(personId);
-				travelTimes.add(String.valueOf(startTime)+"-"+String.valueOf(travelTime));
-				personId2TravelTimes.put(personId, travelTimes);
+	private void storeData(final Id<Person> personId, final String legMode, final double departureTime, final double travelTime){
+		if(this.modePersonIdDepartureTravelTimes.containsKey(legMode)){
+			Map<Id<Person>, List<String>> personIdDepartureTravelTimes = this.modePersonIdDepartureTravelTimes.get(legMode);
+			if(personIdDepartureTravelTimes.containsKey(personId)){
+				List<String> departureTravelTimes = personIdDepartureTravelTimes.get(personId);
+				departureTravelTimes.add(String.valueOf(departureTime)+"-"+String.valueOf(travelTime));
+				personIdDepartureTravelTimes.put(personId, departureTravelTimes);
 			} else {
-				List<String> travelTimes = new ArrayList<>();
-				travelTimes.add(String.valueOf(startTime)+"-"+String.valueOf(travelTime));
-				personId2TravelTimes.put(personId, travelTimes);
+				List<String> departureTravelTimes = new ArrayList<>();
+				departureTravelTimes.add(String.valueOf(departureTime)+"-"+String.valueOf(travelTime));
+				personIdDepartureTravelTimes.put(personId, departureTravelTimes);
 			}
 		} else { 
-			Map<Id<Person>, List<String>> personId2TravelTimes = new HashMap<>();
-			List<String> travelTimes = new ArrayList<>();
-			travelTimes.add(String.valueOf(startTime)+"-"+String.valueOf(travelTime));
-			personId2TravelTimes.put(personId, travelTimes);
-			this.mode2PersonId2TravelTimes.put(legMode, personId2TravelTimes);
+			Map<Id<Person>, List<String>> personIdDepartureTravelTimes = new HashMap<>();
+			List<String> departureTravelTimes = new ArrayList<>();
+			departureTravelTimes.add(String.valueOf(departureTime)+"-"+String.valueOf(travelTime));
+			personIdDepartureTravelTimes.put(personId, departureTravelTimes);
+			this.modePersonIdDepartureTravelTimes.put(legMode, personIdDepartureTravelTimes);
 		}
 	}
-	
+
 }
