efb18c3a58a36b18825796e920ed6d9d7aa232ed
kainagel <nagel@vsp.tu-berlin.de>
2017-07-21 07:47:14 +0200

    comment out some code that is no longer compiling because of an upstream change

--- kai/src/main/java/playground/kai/usecases/opdytsintegration/modechoice/KNModeChoiceCalibMain.java
+++ kai/src/main/java/playground/kai/usecases/opdytsintegration/modechoice/KNModeChoiceCalibMain.java
@@ -139,21 +139,23 @@ class KNModeChoiceCalibMain {
 			} else {
 				convergenceCriterion= new FixedIterationNumberConvergenceCriterion(100, 10 );
 			}
-			RandomSearch<ModeChoiceDecisionVariable> randomSearch = new RandomSearch<>( simulator,
-					new ModeChoiceRandomizer(scenario) ,
-					initialDecisionVariable ,
-					convergenceCriterion ,
-					maxIterations, maxTransitions, populationSize, 
-					MatsimRandom.getRandom(),
-					interpolate,
-					new ModeChoiceObjectiveFunction(equil),
-					includeCurrentBest, warmupIterations ) ;
-
-			randomSearch.setLogPath( outputDirectory );
-
-			// ---
-
-			randomSearch.run(new SelfTuner(0.95));
+			RandomSearch<ModeChoiceDecisionVariable> randomSearch = null ;
+			
+//			new RandomSearch<>( simulator,
+//					new ModeChoiceRandomizer(scenario) ,
+//					initialDecisionVariable ,
+//					convergenceCriterion ,
+//					maxIterations, maxTransitions, populationSize, 
+//					MatsimRandom.getRandom(),
+//					interpolate,
+//					new ModeChoiceObjectiveFunction(equil),
+//					includeCurrentBest, warmupIterations ) ;
+//
+//			randomSearch.setLogPath( outputDirectory );
+//
+//			// ---
+//
+//			randomSearch.run(new SelfTuner(0.95));
 
 		} else {
 			config.controler().setLastIteration(1000);
