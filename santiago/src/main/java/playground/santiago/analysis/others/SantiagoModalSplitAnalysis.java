7a33f0fc482510ac44d0967bbb504d5d87aa14c9
leocamus <leocamusc@gmail.com>
2017-07-21 11:24:35 -0400

    New location

---
+++ santiago/src/main/java/playground/santiago/analysis/others/SantiagoModalSplitAnalysis.java
@@ -0,0 +1,46 @@
+package playground.santiago.analysis.others;
+
+import java.io.File;
+import java.util.List;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.population.Person;
+
+import playground.agarwalamit.analysis.modalShare.ModalShareFromEvents;
+
+
+public class SantiagoModalSplitAnalysis {
+	private String runDir;	
+	private String outputDir;
+	private String analysisDir;
+	private List<Id<Person>> stuckAgents;
+	
+	public SantiagoModalSplitAnalysis(String caseName, String stepName, List<Id<Person>> stuckAgents){
+		this.runDir = "../../../runs-svn/santiago/" + caseName + "/";
+		this.outputDir = runDir + "outputOf" + stepName + "/";
+		this.analysisDir = outputDir + "analysis/";	
+		this.stuckAgents=stuckAgents;
+		
+	}
+	
+	private void createDir(File file) {
+		file.mkdirs();	
+	}
+	
+	
+	/**
+	 * writeModalShare uses ModalShareFromEvents from playground.agarwalamit.analysis.modalShare.ModalShareFromEvents
+	 */
+	public void writeModalShare(int it){
+
+		File analysisDir = new File(this.analysisDir);
+		if(!analysisDir.exists()) createDir(analysisDir);
+		String eventsFile = outputDir + "ITERS/it." + String.valueOf(it) + "/" + String.valueOf(it) + ".events.xml.gz";
+		ModalShareFromEvents msc = new ModalShareFromEvents(eventsFile);
+		msc.run();
+		String outputFile = analysisDir + String.valueOf(it) + ".modalSplit.txt";
+		msc.writeResults(outputFile);
+
+
+	}
+}
