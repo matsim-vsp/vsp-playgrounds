ad4b9273544b23af1a7761e3ebe68c3614dbb12c
leocamus <leocamusc@gmail.com>
2017-07-21 11:27:22 -0400

    Useful for ordering the analysis

---
+++ santiago/src/main/java/playground/santiago/analysis/SantiagoRunAnalysis.java
@@ -0,0 +1,10 @@
+package playground.santiago.analysis;
+
+public class SantiagoRunAnalysis {
+	//Fields related to the scenario and its steps - they must be changed depending on the step . a comment
+	private static final String CASE_NAME = "baseCase1pct";
+	private static final String STEP_NAME = "Step0";
+	private static final int FIRST_IT = 0;
+	private static final int LAST_IT = 100;
+	private static final int LENGTH = LAST_IT - FIRST_IT;
+}
