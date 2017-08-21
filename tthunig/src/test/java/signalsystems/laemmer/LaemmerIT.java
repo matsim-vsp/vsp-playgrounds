02ec3fd6b3d473bede84dd81bffb0e401f9050d0
Theresa Thunig <thunig@vsp.tu-berlin.de>
2017-08-21 18:10:49 +0200

    further extent laemmer test

--- tthunig/src/test/java/signalsystems/laemmer/LaemmerIT.java
+++ tthunig/src/test/java/signalsystems/laemmer/LaemmerIT.java
@@ -28,12 +28,16 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.contrib.signals.model.SignalGroup;
 import org.matsim.contrib.signals.model.SignalSystem;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.testcases.MatsimTestUtils;
 
+import analysis.TtAnalyzedGeneralResultsWriter;
+import analysis.TtGeneralAnalysis;
+import analysis.TtListenerToBindGeneralAnalysis;
 import analysis.signals.TtSignalAnalysisTool;
 import scenarios.illustrative.singleCrossing.SingleCrossingScenario;
 import signals.laemmer.model.LaemmerConfig.Regime;
@@ -55,21 +59,27 @@ public class LaemmerIT {
 	 */
 	@Test
 	public void testSingleCrossingScenarioDemandNS() {
-		TtSignalAnalysisTool signalAnalyzer = runSingleCrossingScenario(1800, 0, true);
+		TtSignalAnalysisTool signalAnalyzer = new TtSignalAnalysisTool();
+		TtGeneralAnalysis generalAnalyzer = runSingleCrossingScenario(1800, 0, true, signalAnalyzer);
 
 		Map<Id<SignalGroup>, Double> totalSignalGreenTimes = signalAnalyzer.getTotalSignalGreenTime(); // NS should show a lot more green than WE
 		Map<Id<SignalGroup>, Double> avgSignalGreenTimePerCycle = signalAnalyzer.calculateAvgSignalGreenTimePerFlexibleCycle(); // WE should be almost 0
 		Map<Id<SignalSystem>, Double> avgCycleTimePerSystem = signalAnalyzer.calculateAvgFlexibleCycleTimePerSignalSystem(); // should be around 60, at most 90
-
+		Map<Id<Link>, Double> avgDelayPerLink = generalAnalyzer.getAvgDelayPerLink();
+		Double avgDelayWE = avgDelayPerLink.get(Id.createLinkId("2_3"));
+		Double avgDelayNS = avgDelayPerLink.get(Id.createLinkId("7_3"));
+		
 		log.info("total signal green times: " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1) + ", "
 				+ totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg signal green times per cycle: " + avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1) + ", "
 				+ avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg cycle time per system: " + avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId));
-
+		log.info("avg delay per link: " + avgDelayWE + ", " + avgDelayNS);
+		
 		Assert.assertNull("signal group 1 should show no green", avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1));
 		Assert.assertNotNull("signal group 2 should show green", avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2));
 		Assert.assertEquals("avg cycle time of the system and total green time of NS-group should be equal", totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2), avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId), MatsimTestUtils.EPSILON);
+		Assert.assertEquals("avg delay at NS-direction should be zero", 0.0, avgDelayNS, MatsimTestUtils.EPSILON);
 	}
 	
 	/**
@@ -78,21 +88,27 @@ public class LaemmerIT {
 	 */
 	@Test
 	public void testSingleCrossingScenarioLowVsHighDemandWithMinG(){
-		TtSignalAnalysisTool signalAnalyzer = runSingleCrossingScenario(90, 1800, true);
+		TtSignalAnalysisTool signalAnalyzer = new TtSignalAnalysisTool();
+		TtGeneralAnalysis generalAnalyzer = runSingleCrossingScenario(90, 1800, true, signalAnalyzer);
 		
 		Map<Id<SignalGroup>, Double> totalSignalGreenTimes = signalAnalyzer.getTotalSignalGreenTime(); 
 		Map<Id<SignalGroup>, Double> avgSignalGreenTimePerCycle = signalAnalyzer.calculateAvgSignalGreenTimePerFlexibleCycle(); 
 		Map<Id<SignalSystem>, Double> avgCycleTimePerSystem = signalAnalyzer.calculateAvgFlexibleCycleTimePerSignalSystem(); 
+		Map<Id<Link>, Double> avgDelayPerLink = generalAnalyzer.getAvgDelayPerLink();
+		Double avgDelayWE = avgDelayPerLink.get(Id.createLinkId("2_3"));
+		Double avgDelayNS = avgDelayPerLink.get(Id.createLinkId("7_3"));
 		
 		log.info("total signal green times: " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1) + ", " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg signal green times per cycle: " + avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1) + ", "
 				+ avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg cycle time per system: " + avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId));
+		log.info("avg delay per link: " + avgDelayWE + ", " + avgDelayNS);
 		
 		Assert.assertTrue("total signal green time of WE-direction should be higher than NS-direction", totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1)-totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2) > 0);
 		Assert.assertTrue("avg signal green time per cycle of WE-direction should be higher than NS-direction", avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1)-avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2) > 0);
 		Assert.assertEquals("avg signal green time per cycle of NS-direction should be the minimum green time of 5 seconds", 5.0, avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2), MatsimTestUtils.EPSILON);
 		Assert.assertTrue("cycle time should stay below 90 seconds", avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId) <= 90);
+		Assert.assertTrue("avg delay per vehicle on WS-direction should be less than on NS-direction", avgDelayWE<avgDelayNS);
 	}
 	
 	/**
@@ -101,24 +117,56 @@ public class LaemmerIT {
 	 */
 	@Test
 	public void testSingleCrossingScenarioLowVsHighDemandWoMinG(){
-		TtSignalAnalysisTool signalAnalyzer = runSingleCrossingScenario(90, 1800, false);
+		TtSignalAnalysisTool signalAnalyzer = new TtSignalAnalysisTool();
+		TtGeneralAnalysis generalAnalyzer = runSingleCrossingScenario(90, 1800, false, signalAnalyzer);
 		
 		Map<Id<SignalGroup>, Double> totalSignalGreenTimes = signalAnalyzer.getTotalSignalGreenTime(); 
 		Map<Id<SignalGroup>, Double> avgSignalGreenTimePerCycle = signalAnalyzer.calculateAvgSignalGreenTimePerFlexibleCycle(); 
 		Map<Id<SignalSystem>, Double> avgCycleTimePerSystem = signalAnalyzer.calculateAvgFlexibleCycleTimePerSignalSystem(); 
+		Map<Id<Link>, Double> avgDelayPerLink = generalAnalyzer.getAvgDelayPerLink();
+		Double avgDelayWE = avgDelayPerLink.get(Id.createLinkId("2_3"));
+		Double avgDelayNS = avgDelayPerLink.get(Id.createLinkId("7_3"));
 		
 		log.info("total signal green times: " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1) + ", " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg signal green times per cycle: " + avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1) + ", "
 				+ avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2));
 		log.info("avg cycle time per system: " + avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId));
+		log.info("avg delay per link: " + avgDelayWE + ", " + avgDelayNS);
 		
 		Assert.assertTrue("total signal green time of WE-direction should be higher than NS-direction", totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1)-totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2) > 0);
 		Assert.assertTrue("avg signal green time per cycle of WE-direction should be higher than NS-direction", avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1)-avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2) > 0);
 		Assert.assertTrue("avg signal green time per cycle of NS-direction should be less than 5 seconds", avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2) < 5.0);
 		Assert.assertTrue("cycle time should stay below 90 seconds", avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId) <= 90);
+		Assert.assertTrue("avg delay per vehicle on WS-direction should be less than on NS-direction", avgDelayWE<avgDelayNS);
 	}
 	
-	private TtSignalAnalysisTool runSingleCrossingScenario(double flowNS, double flowWE, boolean minG) {
+	/**
+	 * directions with the same demand-capacity-ration should get green for more or less the same time
+	 */
+	@Test
+	public void testSingleCrossingScenarioEqualDemandCapacityRatio(){
+		TtSignalAnalysisTool signalAnalyzer = new TtSignalAnalysisTool();
+		TtGeneralAnalysis generalAnalyzer = runSingleCrossingScenario(900, 1800, false, signalAnalyzer);
+		
+		Map<Id<SignalGroup>, Double> totalSignalGreenTimes = signalAnalyzer.getTotalSignalGreenTime(); 
+		Map<Id<SignalGroup>, Double> avgSignalGreenTimePerCycle = signalAnalyzer.calculateAvgSignalGreenTimePerFlexibleCycle(); 
+		Map<Id<SignalSystem>, Double> avgCycleTimePerSystem = signalAnalyzer.calculateAvgFlexibleCycleTimePerSignalSystem(); 
+		Map<Id<Link>, Double> avgDelayPerLink = generalAnalyzer.getAvgDelayPerLink();
+		Double avgDelayWE = avgDelayPerLink.get(Id.createLinkId("2_3"));
+		Double avgDelayNS = avgDelayPerLink.get(Id.createLinkId("7_3"));
+		
+		log.info("total signal green times: " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1) + ", " + totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2));
+		log.info("avg signal green times per cycle: " + avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1) + ", "
+				+ avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2));
+		log.info("avg cycle time per system: " + avgCycleTimePerSystem.get(SingleCrossingScenario.signalSystemId));
+		log.info("avg delay per link: " + avgDelayWE + ", " + avgDelayNS);
+		
+		Assert.assertEquals("total signal green times should not differ more than 1%", 1, totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId1)/totalSignalGreenTimes.get(SingleCrossingScenario.signalGroupId2), 0.01);
+		Assert.assertEquals("avg signal green times per cycle should not differ more than 1%", 1, avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId1)/avgSignalGreenTimePerCycle.get(SingleCrossingScenario.signalGroupId2), 0.01);
+		Assert.assertEquals("avg delay per vehicle per link should not differ more than 1%", 1, avgDelayWE/avgDelayNS, 0.01);
+	}
+
+	private TtGeneralAnalysis runSingleCrossingScenario(double flowNS, double flowWE, boolean minG, TtSignalAnalysisTool signalAnalyzer) {
 		SingleCrossingScenario singleCrossingScenario = new SingleCrossingScenario(flowNS, flowWE, true, Regime.COMBINED, false, false, false, false, true, true, 0, false);
 		if (minG){
 			singleCrossingScenario.setMinG(5);
@@ -127,7 +175,6 @@ public class LaemmerIT {
 		Controler controler = singleCrossingScenario.defineControler();
 		controler.getConfig().controler().setOutputDirectory(testUtils.getOutputDirectory());
 		// add signal analysis tool
-		TtSignalAnalysisTool signalAnalyzer = new TtSignalAnalysisTool();
 		controler.addOverridingModule(new AbstractModule() {
 			@Override
 			public void install() {
@@ -135,26 +182,27 @@ public class LaemmerIT {
 				this.addControlerListenerBinding().toInstance(signalAnalyzer);
 			}
 		});
-		
+		// add general analysis tools
+		TtGeneralAnalysis generalAnalysis = new TtGeneralAnalysis(controler.getScenario().getNetwork());
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				this.addEventHandlerBinding().toInstance(generalAnalysis);
+			}
+		});
+
 		controler.run();
 		
-		return signalAnalyzer;
+		return generalAnalysis;
 	}
 
-	@Test
-	public void testSingleCrossingScenarioPrivilegeWE(){
-		// more demand on WE than on NS
-	}
-	
-	// TODO test green wave (higher capacity wins)
-	
-	// TODO test stabilizing regime
-	// TODO test optimizing regime
+	// TODO test stabilizing regime: only very few agents on NS-direction - what happens?!
+	// TODO test combined regime: only very few agents on NS-direction, have to wait not longer than maximum cycle time
+	// TODO test optimizing regime: only very few agents on NS-direction, have to wait longer than maximum cycle time
 	
 	// TODO somehow test stochasticity (besser als fixed?!; reagiert auf nachfrage - anders als constant)
 	// TODO somehow test temporarily overcrowded situations (no exeption, stau löst sich wieder auf)
 	// TODO somehow test liveArrivalRate vs. exact data (letzteres erzeugt bessere ergebnisse; liveArrivalRates werden korrekt bestimmt)
-	// TODO somehow test minimal green time - wird es eingehalten? wird trotzdem 'bester' richtung grün gegeben? (zur not ergebnis des ganzen runs mit ergebnissen aus nicos MA abgleichen)
 	// TODO somehow test grouping
 	// TODO test lanes
 	// ...
