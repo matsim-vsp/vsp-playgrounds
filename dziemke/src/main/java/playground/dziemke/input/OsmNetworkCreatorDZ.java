426a48984a76ddee3be757179a7569850b54ea30
dziemke <ziemke@vsp.tu-berlin.de>
2017-10-11 15:24:22 +0200

    minor

--- dziemke/src/main/java/playground/dziemke/input/OsmNetworkCreatorDZ.java
+++ dziemke/src/main/java/playground/dziemke/input/OsmNetworkCreatorDZ.java
@@ -29,19 +29,21 @@ public class OsmNetworkCreatorDZ {
 		// String osmFileName = "../../nemo/data/input/counts/verkehrszaehlung_2015/network/allWaysNRW.osm";
 		// String outputRoot = "../../nemo/data/input/network/motorway-tertiary/";
 		// String networkFileName = outputRoot + "network_motorway-tertiary.xml.gz";
-		String osmFileName = "../../upretoria/data/capetown/osm/2017-10-03";
-		String outputRoot = "../../upretoria/data/capetown/network/";
-		String networkFileName = outputRoot + "2017-10-03_network.xml.gz";
+		String osmFileName = "../../capetown/data/osm/2017-10-03";
+		String outputRoot = "../../capetown/data/network/";
+		String networkFileName = outputRoot + "2017-10-03_network_motorway_secondary.xml.gz";
 
 		// EPSG:4326 = WGS84
 		// EPSG:31468 = DHDN GK4, for Berlin; DE
 		// EPSG:26918 = NAD83 / UTM zone 18N, for Maryland, US
 		// EPSG:25832 = ETRS89 / UTM zone 32N, for Nordrhein-Westfalen
-		// EPSG:22235 = Cape / UTM zone 35S, for South Africa
-		String outputCRS = "EPSG:22235";
+		// EPSG:32734 = WGS 84 / UTM zone 34S, for South Africa
+		String outputCRS = "EPSG:32734";
 		
 		AccessibilityOsmNetworkReader osmNetworkCreatorDZ = new AccessibilityOsmNetworkReader(osmFileName, outputCRS);
+		osmNetworkCreatorDZ.setOnlyMotorwayToSecondary(true);
 		osmNetworkCreatorDZ.createNetwork();
+		
 		osmNetworkCreatorDZ.writeNetwork(outputRoot, networkFileName);
 	}
 }
