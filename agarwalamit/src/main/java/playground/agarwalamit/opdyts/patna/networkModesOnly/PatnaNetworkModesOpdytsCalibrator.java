7f3ff020e737859c6ae9d3e665b7f98c4e92e9a2
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-10-25 20:44:58 +0200

    preparing patna network modes opdyts calibrator for runs

--- agarwalamit/src/main/java/playground/agarwalamit/opdyts/patna/networkModesOnly/PatnaNetworkModesOpdytsCalibrator.java
+++ agarwalamit/src/main/java/playground/agarwalamit/opdyts/patna/networkModesOnly/PatnaNetworkModesOpdytsCalibrator.java
@@ -20,11 +20,15 @@
 package playground.agarwalamit.opdyts.patna.networkModesOnly;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import com.google.common.io.Files;
 import floetteroed.opdyts.DecisionVariableRandomizer;
 import floetteroed.opdyts.ObjectiveFunction;
+import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.opdyts.MATSimSimulator2;
 import org.matsim.contrib.opdyts.MATSimStateFactoryImpl;
@@ -35,7 +39,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.AbstractModule;
-import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.controler.events.ShutdownEvent;
 import org.matsim.core.controler.listener.ShutdownListener;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -45,8 +49,14 @@ import playground.agarwalamit.analysis.modalShare.ModalShareControlerListener;
 import playground.agarwalamit.analysis.modalShare.ModalShareEventHandler;
 import playground.agarwalamit.analysis.tripTime.ModalTravelTimeControlerListener;
 import playground.agarwalamit.analysis.tripTime.ModalTripTravelTimeHandler;
-import playground.agarwalamit.opdyts.*;
+import playground.agarwalamit.opdyts.DistanceDistribution;
+import playground.agarwalamit.opdyts.ModeChoiceDecisionVariable;
+import playground.agarwalamit.opdyts.ModeChoiceObjectiveFunction;
+import playground.agarwalamit.opdyts.ModeChoiceRandomizer;
+import playground.agarwalamit.opdyts.OpdytsScenario;
+import playground.agarwalamit.opdyts.RandomizedUtilityParametersChoser;
 import playground.agarwalamit.opdyts.analysis.OpdytsModalStatsControlerListener;
+import playground.agarwalamit.opdyts.equil.MatsimOpdytsEquilMixedTrafficIntegration;
 import playground.agarwalamit.opdyts.plots.BestSolutionVsDecisionVariableChart;
 import playground.agarwalamit.opdyts.plots.OpdytsConvergenceChart;
 import playground.agarwalamit.utils.FileUtils;
@@ -65,16 +75,27 @@ public class PatnaNetworkModesOpdytsCalibrator {
 		String relaxedPlans ;
 		ModeChoiceRandomizer.ASCRandomizerStyle ascRandomizeStyle;
 
+		double stepSize = 1.0;
+		int iterations2Convergence = 800;
+		double selfTuningWt = 1.0;
+		int warmUpItrs = 5;
+
 		if ( args.length>0 ) {
 			configFile = args[0];
 			OUT_DIR = args[1];
 			relaxedPlans = args[2];
 			ascRandomizeStyle = ModeChoiceRandomizer.ASCRandomizerStyle.valueOf(args[3]);
+
+			// opdyts params
+			stepSize = Double.valueOf(args[4]);
+			iterations2Convergence = Integer.valueOf(args[5]);
+			selfTuningWt = Double.valueOf(args[6]);
+			warmUpItrs = Integer.valueOf(args[7]);
 		} else {
-			configFile = FileUtils.RUNS_SVN+"/opdyts/patna/input_networkModes/"+"/config_networkModesOnly.xml";
-			OUT_DIR = FileUtils.RUNS_SVN+"/opdyts/patna/output_networkModes/calib_trial/";
-			relaxedPlans = FileUtils.RUNS_SVN+"/opdyts/patna/output_networkModes/initialPlans2RelaxedPlans/output_plans.xml.gz";
-			ascRandomizeStyle = ModeChoiceRandomizer.ASCRandomizerStyle.axial_randomVariation;
+			configFile = FileUtils.RUNS_SVN+"/opdyts/patna/networkModes/calibration/input/config_networkModesOnly.xml";
+			OUT_DIR = FileUtils.RUNS_SVN+"/opdyts/patna/networkModes/calibration/output/";
+			relaxedPlans = FileUtils.RUNS_SVN+"/opdyts/patna/networkModes/relaxedPlans/output/output_plans.xml.gz";
+			ascRandomizeStyle = ModeChoiceRandomizer.ASCRandomizerStyle.axial_fixedVariation;
 		}
 
 		Config config = ConfigUtils.loadConfig(configFile, new OpdytsConfigGroup());
@@ -82,16 +103,28 @@ public class PatnaNetworkModesOpdytsCalibrator {
 		config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn); // must be warn, since opdyts override few things
 		config.controler().setOutputDirectory(OUT_DIR);
 
-		OpdytsConfigGroup opdytsConfigGroup = (OpdytsConfigGroup) config.getModules().get(OpdytsConfigGroup.GROUP_NAME);
+		// from GF, every run should have a different random seed.
+		int randomSeed = new Random().nextInt(9999);
+		config.global().setRandomSeed(randomSeed);
+
+		OpdytsConfigGroup opdytsConfigGroup = ConfigUtils.addOrGetModule(config, OpdytsConfigGroup.GROUP_NAME, OpdytsConfigGroup.class ) ;
 		opdytsConfigGroup.setOutputDirectory(OUT_DIR);
-
-		Scenario scenario = ScenarioUtils.loadScenario(config);
-		scenario.getConfig().controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
-
-		// == opdyts settings
-		MATSimOpdytsControler<ModeChoiceDecisionVariable> opdytsControler = new MATSimOpdytsControler<>(scenario);
+		opdytsConfigGroup.setVariationSizeOfRandomizeDecisionVariable(stepSize);
+		opdytsConfigGroup.setNumberOfIterationsForConvergence(iterations2Convergence);
+		opdytsConfigGroup.setSelfTuningWeight(selfTuningWt);
+		opdytsConfigGroup.setWarmUpIterations(warmUpItrs);
 
 		List<String> modes2consider = Arrays.asList("car","bike","motorbike");
+
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+		config.strategy().setFractionOfIterationsToDisableInnovation(Double.POSITIVE_INFINITY);
+
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		//****************************** mainly opdyts settings ******************************
+
+		MATSimOpdytsControler<ModeChoiceDecisionVariable> opdytsControler = new MATSimOpdytsControler<>(scenario);
+
 		DistanceDistribution referenceStudyDistri = new PatnaNetworkModesOneBinDistanceDistribution(PATNA_1_PCT);
 		OpdytsModalStatsControlerListener stasControlerListner = new OpdytsModalStatsControlerListener(modes2consider,referenceStudyDistri);
 
@@ -117,6 +150,28 @@ public class PatnaNetworkModesOpdytsCalibrator {
 				addControlerListenerBinding().toInstance(new ShutdownListener() {
 					@Override
 					public void notifyShutdown(ShutdownEvent event) {
+						// copy the state vector elements files before removing ITERS dir
+						String outDir = event.getServices().getControlerIO().getOutputPath()+"/vectorElementSizeFiles/";
+						new File(outDir).mkdirs();
+
+						int firstIt = event.getServices().getConfig().controler().getFirstIteration();
+						int lastIt = event.getServices().getConfig().controler().getLastIteration();
+						int plotEveryItr = 50;
+
+						for (int itr = firstIt+1; itr <=lastIt; itr++) {
+							if ( (itr == firstIt+1 || itr%plotEveryItr ==0) && new File(event.getServices().getControlerIO().getIterationPath(itr)).exists() ) {
+								{
+									String sourceFile = event.getServices().getControlerIO().getIterationFilename(itr,"stateVector_networkModes.txt");
+									String sinkFile =  outDir+"/"+itr+".stateVector_networkModes.txt";
+									try {
+										Files.copy(new File(sourceFile), new File(sinkFile));
+									} catch (IOException e) {
+										Logger.getLogger(MatsimOpdytsEquilMixedTrafficIntegration.class).warn("Data is not copied. Reason : " + e);
+									}
+								}
+							}
+						}
+
 						// remove the unused iterations
 						String dir2remove = event.getServices().getControlerIO().getOutputPath()+"/ITERS/";
 						IOUtils.deleteDirectoryRecursively(new File(dir2remove).toPath());
