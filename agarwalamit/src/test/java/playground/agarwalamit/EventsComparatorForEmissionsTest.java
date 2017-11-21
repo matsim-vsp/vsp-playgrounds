2a21091b7f0af4646ec405800d94cffff09674d7
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-11-21 22:18:56 +0100

    adapting test

--- agarwalamit/src/test/java/playground/agarwalamit/EventsComparatorForEmissionsTest.java
+++ agarwalamit/src/test/java/playground/agarwalamit/EventsComparatorForEmissionsTest.java
@@ -20,12 +20,18 @@
 package playground.agarwalamit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.ActivityEndEvent;
 import org.matsim.api.core.v01.events.ActivityStartEvent;
@@ -37,19 +43,84 @@ import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
 import org.matsim.api.core.v01.events.VehicleEntersTrafficEvent;
 import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
+import org.matsim.api.core.v01.events.handler.VehicleEntersTrafficEventHandler;
+import org.matsim.api.core.v01.events.handler.VehicleLeavesTrafficEventHandler;
 import org.matsim.contrib.emissions.events.ColdEmissionEvent;
+import org.matsim.contrib.emissions.events.ColdEmissionEventHandler;
 import org.matsim.contrib.emissions.events.WarmEmissionEvent;
+import org.matsim.contrib.emissions.events.WarmEmissionEventHandler;
 import org.matsim.contrib.emissions.types.ColdPollutant;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
 import org.matsim.facilities.ActivityFacility;
+import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.vehicles.Vehicle;
 import playground.agarwalamit.emissions.onRoadExposure.EventsComparatorForEmissions;
+import playground.agarwalamit.emissions.onRoadExposure.EventsComparatorForEmissions.EventsOrder;
+import playground.kai.usecases.combinedEventsReader.CombinedMatsimEventsReader;
 
 /**
  * Created by amit on 20.11.17.
  */
 
+@RunWith(Parameterized.class)
 public class EventsComparatorForEmissionsTest {
 
+    @Rule
+    public MatsimTestUtils helper = new MatsimTestUtils();
+
+    private final EventsOrder eventsOrder;
+
+    public EventsComparatorForEmissionsTest( EventsOrder eventsOrder ) {
+        this.eventsOrder = eventsOrder;
+    }
+
+    @Parameterized.Parameters(name = "{index}: EventsOrder == {0};")
+    public static Collection<Object> getCases(){
+        return Arrays.asList(
+                EventsOrder.NATURAL_ORDER,
+                EventsOrder.EMISSION_EVENTS_BEFORE_LINK_LEAVE_EVENT);
+    }
+
+	@Test
+	public void testFromFile() {
+		String file = helper.getClassInputDirectory()+"/testEvents.xml";
+		EventsManager events = EventsUtils.createEventsManager();
+		CombinedMatsimEventsReader reader = new CombinedMatsimEventsReader(events);
+		MyHandler myHandler = new MyHandler();
+		events.addHandler(myHandler);
+		reader.readFile(file);
+		
+		List<Event> eventsList = myHandler.events;
+        Collections.sort(eventsList, new EventsComparatorForEmissions(this.eventsOrder));
+        eventsList.stream().forEach(e -> System.out.println(e.toString()));
+
+//        Map<String, List<Event>> eventsMap = eventsList.stream().collect( Collectors.groupingBy(Event::getEventType));
+
+        Assert.assertEquals("Wrong positions of events ",
+                "[0, 1, 2]",
+                Arrays.toString(IntStream.range(0, eventsList.size())
+                                         .filter(i -> eventsList.get(i).getEventType().equals(VehicleEntersTrafficEvent.EVENT_TYPE))
+                                         .toArray()));
+
+        if (this.eventsOrder.equals(EventsOrder.EMISSION_EVENTS_BEFORE_LINK_LEAVE_EVENT)) {
+            Assert.assertEquals("Wrong positions of events ",
+                    "[3, 4, 5, 6, 7, 8]",
+                    Arrays.toString(IntStream.range(0, eventsList.size())
+                                             .filter(i -> eventsList.get(i).getEventType().equals(ColdEmissionEvent.EVENT_TYPE))
+                                             .toArray()));
+
+            Assert.assertEquals("Wrong positions of events ",
+                    "["+String.valueOf((int) (eventsList.size()-1) )+"]",
+                    Arrays.toString(IntStream.range(0, eventsList.size())
+                                             .filter(i -> eventsList.get(i).getEventType().equals(VehicleLeavesTrafficEvent.EVENT_TYPE))
+                                             .toArray()));
+        }
+
+	}
+	
     @Test
     public void test(){
 
@@ -74,7 +145,7 @@ public class EventsComparatorForEmissionsTest {
         System.out.println("Events before sorting ...");
         eventList.stream().forEach(e -> System.out.println(e.toString()));
 
-        Collections.sort(eventList, new EventsComparatorForEmissions());
+        Collections.sort(eventList, new EventsComparatorForEmissions(this.eventsOrder));
 
         System.out.println("Events after sorting ...");
 
@@ -94,14 +165,18 @@ public class EventsComparatorForEmissionsTest {
         eventList.add(new LinkEnterEvent(151.0, Id.createVehicleId("OC7_X2P_E2I_7937_truck"),Id.createLinkId("2205-220110000-1964-2768-2788-2785-278310000-2792-2742-2841") ));
 
         System.out.println("Events after sorting ...");
-        Collections.sort(eventList, new EventsComparatorForEmissions());
+        Collections.sort(eventList, new EventsComparatorForEmissions(this.eventsOrder));
         eventList.stream().forEach(e -> System.out.println(e.toString()));
 
         event = eventList.stream().filter(e->e.toString().contains(WarmEmissionEvent.EVENT_TYPE)).findFirst().get();
 
-        Assert.assertEquals("Wrong event position", 2, eventList.indexOf(event));
+        if (this.eventsOrder==EventsOrder.NATURAL_ORDER) {
+            Assert.assertEquals("Wrong event position", 4, eventList.indexOf(event));
+        } else {
+            Assert.assertEquals("Wrong event position", 0, eventList.indexOf(event));
+        }
 
-        //
+        // arrival-departure and vehicleLeaveTraffic-vehicleEnterTraffic events in same time step
         eventList.clear();
         eventList.add(new VehicleLeavesTrafficEvent(10.0,Id.createPersonId("OC7_X2P_E2I_7942"),Id.createLinkId("1973"),Id.create("OC7_X2P_E2I_7942_truck", Vehicle.class),"truck", 1.0));
         eventList.add(new PersonArrivalEvent(10.0, Id.createPersonId("OC7_X2P_E2I_7942"),Id.createLinkId("1973"), "truck"));
@@ -112,12 +187,66 @@ public class EventsComparatorForEmissionsTest {
 
         System.out.println("Events before sorting ...");
         eventList.stream().forEach(e -> System.out.println(e.toString()));
-        Collections.sort(eventList, new EventsComparatorForEmissions());
+        Collections.sort(eventList, new EventsComparatorForEmissions(this.eventsOrder));
         System.out.println("Events after sorting ...");
-
         eventList.stream().forEach(e -> System.out.println(e.toString()));
+        
         event = eventList.stream().filter(e->e.toString().contains(VehicleEntersTrafficEvent.EVENT_TYPE)).findFirst().get();
 
-        Assert.assertEquals("Wrong event position", 5, eventList.indexOf(event));
+        if (this.eventsOrder==EventsOrder.NATURAL_ORDER) {
+            Assert.assertEquals("Wrong event position", 2, eventList.indexOf(event));
+        } else {
+            Assert.assertEquals("Wrong event position", 4, eventList.indexOf(event));
+        }
+        
+        //  same event type
+        eventList.clear();
+        
+        eventList.add(new WarmEmissionEvent(151.0,Id.createLinkId("2206"),Id.createVehicleId("OC7_X2P_E2I_7937_truck"), new HashMap<>()));
+        eventList.add(new WarmEmissionEvent(151.0,Id.createLinkId("bike_2206"),Id.createVehicleId("OC7_X2P_E2I_7937_truck"), new HashMap<>()));
+        
+        System.out.println("Events before sorting ...");
+        eventList.stream().forEach(e -> System.out.println(e.toString()));
+        Collections.sort(eventList, new EventsComparatorForEmissions(this.eventsOrder));
+        System.out.println("Events after sorting ...");
+        eventList.stream().forEach(e -> System.out.println(e.toString()));
+        
+        event = eventList.stream().filter(e->e.toString().contains("bike_2206")).findFirst().get();
+        Assert.assertEquals("Wrong event position", 1, eventList.indexOf(event));
+    }
+
+    private class MyHandler implements LinkLeaveEventHandler, LinkEnterEventHandler, ColdEmissionEventHandler, WarmEmissionEventHandler, VehicleEntersTrafficEventHandler, VehicleLeavesTrafficEventHandler {
+
+        List<Event> events = new ArrayList<>();
+
+        @Override
+        public void handleEvent(LinkLeaveEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public void handleEvent(VehicleLeavesTrafficEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public void handleEvent(VehicleEntersTrafficEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public void handleEvent(ColdEmissionEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public void handleEvent(LinkEnterEvent event) {
+            events.add(event);
+        }
+
+        @Override
+        public void handleEvent(WarmEmissionEvent event) {
+            events.add(event);
+        }
     }
 }
