b33a025115862e68f668803269b49fcf0405cd65
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2017-11-24 10:35:47 +0100

    minor

--- agarwalamit/src/main/java/playground/agarwalamit/mixedTraffic/patnaIndia/OnRoadExposure/PatnaMatsimNetwork2Shape.java
+++ agarwalamit/src/main/java/playground/agarwalamit/mixedTraffic/patnaIndia/OnRoadExposure/PatnaMatsimNetwork2Shape.java
@@ -19,8 +19,14 @@
 
 package playground.agarwalamit.mixedTraffic.patnaIndia.OnRoadExposure;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.utils.io.IOUtils;
 import playground.agarwalamit.mixedTraffic.patnaIndia.utils.PatnaUtils;
 import playground.agarwalamit.utils.FileUtils;
+import playground.agarwalamit.utils.LoadMyScenarios;
 import playground.agarwalamit.utils.networkProcessing.MatsimNetwork2Shape;
 
 /**
@@ -31,11 +37,28 @@ public class PatnaMatsimNetwork2Shape {
 
     public static void main(String[] args) {
 
-        String matsimNetwork = FileUtils.RUNS_SVN+"/patnaIndia/run108/jointDemand/policies/0.15pcu/BT-b/output_network.xml.gz";
+        String matsimNetwork = FileUtils.RUNS_SVN+"/patnaIndia/run108/jointDemand/policies/0.15pcu/bau/output_network.xml.gz";
         String shapeDir = FileUtils.RUNS_SVN+"/patnaIndia/run111/onRoadExposure/analysis/networkShape/";
         String epsg = PatnaUtils.EPSG;
 
         MatsimNetwork2Shape.main(new String[] {matsimNetwork, shapeDir, epsg});
+        new PatnaMatsimNetwork2Shape().writeLinkCentroid(matsimNetwork, shapeDir);
+    }
+
+    private void writeLinkCentroid(String networkFile, String outputFilesDir){
+        BufferedWriter writer = IOUtils.getBufferedWriter(outputFilesDir+"/linkIdToCentroid.txt");
+        Network network = LoadMyScenarios.loadScenarioFromNetwork(networkFile).getNetwork();
+
+        try {
+            writer.write("linkId\tcentroidX\tcentoridY\n");
+            for (Link l : network.getLinks().values()) {
+                writer.write(l.getId().toString()+"\t"+l.getCoord().getX()+"\t"+l.getCoord().getY()+"\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Data is not written/read. Reason : " + e);
+        }
+
     }
 
 }
