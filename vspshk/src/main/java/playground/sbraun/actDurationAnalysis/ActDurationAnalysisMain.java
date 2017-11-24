0bf3f1ee6a7e2ba8012569fbda6ff81ee16a7f92
soehnkebraun <braun@vsp.tu-berlin.de>
2017-11-24 16:11:05 +0100

    Revert "-minor fix"
    This reverts commit b80309862d345fa894c57c807a820cdfdc29b149.

--- vspshk/src/main/java/playground/sbraun/actDurationAnalysis/ActDurationAnalysisMain.java
+++ vspshk/src/main/java/playground/sbraun/actDurationAnalysis/ActDurationAnalysisMain.java
@@ -1,35 +1,39 @@
 package playground.sbraun.actDurationAnalysis;
 
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
+import org.matsim.core.scenario.ScenarioUtils;
 
 
-/**
- * @author Soehnke
- *
- */
 
-public class ActDurationAnalysisMain {
+public class IKAnalysisMain1 {
 
-	static String eventsFile = "C://Users//braun//Documents//VSP//Skillbuilding//example1//Sample//output//run01//output_events.xml.gz";
+	static String configFile = "C:\\Users\\braun\\Documents\\VSP\\Skillbuilding\\example1\\Sample\\config.xml";
+	static String eventsFile = "C:\\Users\\braun\\Documents\\VSP\\Skillbuilding\\example1\\Sample\\output\\run01\\output_events.xml.gz";
 				
 	public static void main(String[] args) {
-		ActDurationAnalysisMain anaMain = new ActDurationAnalysisMain();
+		IKAnalysisMain1 anaMain = new IKAnalysisMain1();
 		anaMain.run();
 	}
 
 	private void run() {
 	
+		Config config = ConfigUtils.loadConfig(configFile);
+		Scenario scenario = ScenarioUtils.loadScenario(config);
 		EventsManager events = EventsUtils.createEventsManager();
-		ActDurationHandler handler1 = new ActDurationHandler();
+		
+		IKEventHandler handler1 = new IKEventHandler(scenario.getNetwork());
 		events.addHandler(handler1);
 		// add more handlers here
 		
 		MatsimEventsReader reader = new MatsimEventsReader(events);
 		reader.readFile(eventsFile);
 		
-		handler1.output("C://Users//braun//Documents//VSP//Skillbuilding//example1//Sample//ActivityTimes.csv");
+		handler1.output("C:\\\\Users\\\\braun\\\\Documents\\\\VSP\\\\Skillbuilding\\example1\\Sample\\ActivityTimes.csv");
 					
 	}
 			 
--- vspshk/src/main/java/playground/sbraun/actDurationAnalysis/ActDurationHandler.java
+++ vspshk/src/main/java/playground/sbraun/actDurationAnalysis/ActDurationHandler.java
@@ -26,6 +26,7 @@ package playground.sbraun.actDurationAnalysis;
 
 import org.matsim.api.core.v01.events.handler.ActivityEndEventHandler;
 import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.api.core.v01.events.ActivityStartEvent;
@@ -40,37 +41,42 @@ import java.io.IOException;
 
 
 /**
- * @author Soehnke
+ * @author Ihab
  *
  */
-public class ActDurationHandler implements ActivityStartEventHandler,ActivityEndEventHandler {
+public class IKEventHandler implements ActivityStartEventHandler,ActivityEndEventHandler {
 
-	Map<Id<Person>,Double> personId2starttime = new HashMap<>();
-	Map<Id<Person>,String> personId2acttype = new HashMap<>();
+	Map<Id<Person>,Double> duration_la = new HashMap<>();
+	Map<Id<Person>,String> Activitys = new HashMap<>();
 	List<String> activityChain = new ArrayList<>();
-	//Network network;
+	String agent_activity;
+	double duration;
+	Network network;
 
+	
+	
+	public IKEventHandler(Network network) {
+		this.network = network;
+	}
 
 
 	@Override
 	public void reset(int iteration) {
-		personId2acttype.clear();
-		personId2starttime.clear();
-		activityChain.clear();
+
 	}
 
 	
 	
 	public void handleEvent(ActivityStartEvent event) {
 		if(!event.getActType().equals("pt interaction")) {
-			personId2starttime.put(event.getPersonId(), event.getTime());
-			personId2acttype.put(event.getPersonId(), event.getActType());
+			duration_la.put(event.getPersonId(), event.getTime());
+			Activitys.put(event.getPersonId(), event.getActType());
 		}
 	}
 	
 	public void handleEvent(ActivityEndEvent event) {
-		if((personId2acttype.get(event.getPersonId())!=null || personId2starttime.get(event.getPersonId())!=null)&& !event.getActType().equals("pt interaction")) {
-			String agent_activity = event.getPersonId() +";"+ personId2acttype.get(event.getPersonId()) + ";" +(event.getTime()-personId2starttime.get(event.getPersonId()))+"sec;";
+		if((Activitys.get(event.getPersonId())!=null || duration_la.get(event.getPersonId())!=null)&& !event.getActType().equals("pt interaction")) {
+			String agent_activity = event.getPersonId() +";"+Activitys.get(event.getPersonId()) + ";" +(event.getTime()-duration_la.get(event.getPersonId()))+"sec;";
 			activityChain.add(agent_activity);
 		}
 	}
@@ -78,7 +84,7 @@ public class ActDurationHandler implements ActivityStartEventHandler,ActivityEnd
 	public void output(String filename){
 		BufferedWriter bw = IOUtils.getBufferedWriter(filename);
 		try {
-			bw.write("AgentId ; Activitytype ; Duration");
+			bw.write("AgentId ; activitytype ; Duration");
 			bw.newLine();
 		
 			for (String line : activityChain){
