8eb15fbf7dc024d20030919e4fd82b2161ff9490
pschade <pschade@mailbox.tu-berlin.de>
2018-02-08 03:27:55 +0100

    updating run

--- tthunig/src/main/java/scenarios/illustrative/singleCrossing/RunSingleCrossingScenarioWithFullyAdaptiveLaemmer.java
+++ tthunig/src/main/java/scenarios/illustrative/singleCrossing/RunSingleCrossingScenarioWithFullyAdaptiveLaemmer.java
@@ -1,5 +1,6 @@
 package scenarios.illustrative.singleCrossing;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.matsim.core.controler.Controler;
 
@@ -13,10 +14,10 @@ public class RunSingleCrossingScenarioWithFullyAdaptiveLaemmer {
     private static final Logger log = Logger.getLogger(LaemmerMain.class);
 
     private static final boolean USE_LAEMMER = true;
-    private static final Regime LAEMMER_REGIME = Regime.OPTIMIZING;
+    private static final Regime LAEMMER_REGIME = Regime.STABILIZING;
     
     private static final boolean VISUALIZE_WITH_OTFVIS = true;
-    private static final boolean LOG_ENABLED = false;
+    private static final boolean LOG_ENABLED = true;
     private static final boolean LIVE_ARRIVAL_RATES = true;
     private static final boolean STOCHASTIC_DEMAND = false;
     private static final boolean USE_LANES = true;
@@ -26,6 +27,7 @@ public class RunSingleCrossingScenarioWithFullyAdaptiveLaemmer {
     private static final int MIN_G = 5;
     
     public static void main(String[] args) {
+    	Logger.getRootLogger().setLevel(Level.WARN);
     	if (USE_LAEMMER){    	
     		log.info("Running single crossing scenario with laemmer signals...");
     	} else {
@@ -34,12 +36,13 @@ public class RunSingleCrossingScenarioWithFullyAdaptiveLaemmer {
         
         double flowNS = 180;
         double flowWE = 1450;
-//        for (int i = 0; i <= 3600; i += 60) {
+//        for (int i = 0; i <= 3600; i += 600) {
+//        for (int i = 0; i <= 10; i++) {
 //        	flowWE = i;
             ComplexSingleCrossingScenario complexSingleCrossingSc = new ComplexSingleCrossingScenario(flowNS, 0.0, flowWE, 0.15, USE_LAEMMER, LAEMMER_REGIME, VISUALIZE_WITH_OTFVIS, LOG_ENABLED, STOCHASTIC_DEMAND, USE_LANES, LIVE_ARRIVAL_RATES, GROUPED, MIN_G, TEMPORAL_CROWD);
 			Controler singleCrossingScenario2Controler = complexSingleCrossingSc.defineControler();
-            singleCrossingScenario2Controler.run();
-//        }
+			singleCrossingScenario2Controler.run();
+//		 }
     }
 
 }
