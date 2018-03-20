a7faeed91da76a67afc437d43d91aeb696aae353
gabrielthunig <gabriel.thunig@web.de>
2018-03-20 16:24:09 +0100

    update restrictions

--- vspshk/src/main/java/playground/gthunig/cemdapSpecificationFileModifier/CemdapSpecificationFileModifier.java
+++ vspshk/src/main/java/playground/gthunig/cemdapSpecificationFileModifier/CemdapSpecificationFileModifier.java
@@ -21,8 +21,16 @@ public class CemdapSpecificationFileModifier {
                 int id = Integer.parseInt(idString);
                 if ((id > 5 && id < 10000) ||
                     (id > 10009 && id < 20000) ||
-                    (id > 20000 && id < 30000)) {
-                    System.out.println("Delete line with id " + id);
+                    (id > 20009 && id < 30000)) {
+                    System.out.println("Change line value with id " + id);
+                    String[] lineSplit = line.split("value=\"");
+                    StringBuilder newLine = new StringBuilder(lineSplit[0] + "value=\"" + "0");
+                    String[] suffix = lineSplit[1].split("\"");
+                    for (int i = 1; i < suffix.length; i++) {
+                        newLine.append("\"").append(suffix[i]);
+                    }
+                    bufferedWriter.write(newLine.toString());
+                    bufferedWriter.newLine();
                 } else {
                     bufferedWriter.write(line);
                     bufferedWriter.newLine();
