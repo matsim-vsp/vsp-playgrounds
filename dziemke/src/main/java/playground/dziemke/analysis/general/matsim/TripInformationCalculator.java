70f4d6a5c5705e9036b1e1e2db714c9b0e86b90f
gabrielthunig <gabriel.thunig@web.de>
2018-06-05 16:53:40 +0200

    new signature

--- dziemke/src/main/java/playground/dziemke/analysis/general/matsim/TripInformationCalculator.java
+++ dziemke/src/main/java/playground/dziemke/analysis/general/matsim/TripInformationCalculator.java
@@ -1,6 +1,7 @@
 package playground.dziemke.analysis.general.matsim;
 
 import org.matsim.api.core.v01.network.Network;
+import playground.dziemke.analysis.Trip;
 import playground.dziemke.utils.ProgressLogger;
 
 import java.util.Collection;
@@ -12,15 +13,24 @@ import java.util.List;
 class TripInformationCalculator {
     public static final ProgressLogger log = ProgressLogger.getLogger(Events2TripsParser.class);
 
-    void calculateInformation(List<MatsimTrip> trips, Network network, Collection<String> networkModes) {
+    private Network network;
+    private Collection<String> networkModes;
+
+    public TripInformationCalculator(Network network, Collection<String> networkModes) {
+
+        this.network = network;
+        this.networkModes = networkModes;
+    }
+
+    public void calculateInformation(List<MatsimTrip> trips) {
         log.initializeProgress("Trip", trips.size());
         for (MatsimTrip trip : trips) {
             log.processCounter();
-            calculateInformation(trip, network, networkModes);
+            calculateInformation(trip);
         }
     }
 
-    private static void calculateInformation(MatsimTrip trip, Network network, Collection<String> networkModes) {
+    public void calculateInformation(MatsimTrip trip) {
         trip.setDuration_s(MatsimTripUtils.getDurationByCalculation_s(trip));
         trip.setDistanceBeeline_m(MatsimTripUtils.calculateBeelineDistance_m(trip, network));
         trip.setDistanceRouted_m(MatsimTripUtils.getDistanceRoutedByCalculation_m(trip, network, networkModes));
