3b0c94d673fc52e6995b52ff37f87fba1c55aedb
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-08-02 00:20:03 +0200

    change trip mode car --> taxi

--- ikaddoura/src/main/java/playground/ikaddoura/optAV/prepare/TagCarAndSAVusers.java
+++ ikaddoura/src/main/java/playground/ikaddoura/optAV/prepare/TagCarAndSAVusers.java
@@ -31,6 +31,8 @@ import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -58,8 +60,9 @@ public class TagCarAndSAVusers {
 	
 	private final String inputPlansFile = "berlin-5.1-10pct_plans.xml.gz";
 	private final String outputPlansFile = "berlin-5.1-10pct_plans_taggedCarUsers.xml.gz";
-	
-	private final String outputPersonAttributesFile = "berlin-5.0_person-attributes_potentialSAVusers_10pct.xml.gz";
+	private final String outputPlansFileChangeTripMode = "berlin-5.1-10pct_plans_taggedCarUsers_berlin-population-taxi.xml.gz";
+
+	private final String outputPersonAttributesFile = "berlin-5.0-10pct_person-attributes_potentialSAVusers.xml.gz";
 
 	private final String areaOfPotentialSAVusersSHPFile = "/Users/ihab/Documents/workspace/shared-svn/projects/audi_av/shp/untersuchungsraumAll.shp";
 	private final String crsSHPFile = "EPSG:25833";
@@ -69,13 +72,11 @@ public class TagCarAndSAVusers {
 	private final String inputPersonAttributesFile = "berlin-5.0_person-attributes.xml.gz";
 	private final String inputPersonAttributesSubpopulationPerson = "person";
 	
-	// ####################################################################
-		
-	private String inputPlans;
-	private String outputPlans;
-	private String inputPersonAttributes;
-	private String outputPersonAttributes;
+	// Optional: change trip mode for all potential SAV users
+	private final boolean changeCar2TaxiTripsForAllPotentialSAVusers = true;
 	
+	// ####################################################################
+
 	private Scenario scenario;
 	
 	private int potentialSAVusers = 0;
@@ -90,15 +91,10 @@ public class TagCarAndSAVusers {
 	}
 
 	private void run() {
-		
-		inputPlans = inputDirectory + inputPlansFile;
-		outputPlans = outputDirectory + outputPlansFile;
-		inputPersonAttributes = inputDirectory + inputPersonAttributesFile;
-		outputPersonAttributes = outputDirectory + outputPersonAttributesFile;
-		
+				
 		Config config = ConfigUtils.createConfig();
-		config.plans().setInputFile(inputPlans);
-		if (inputPersonAttributesFile != null) config.plans().setInputPersonAttributeFile(inputPersonAttributes);
+		config.plans().setInputFile(inputDirectory + inputPlansFile);
+		if (inputPersonAttributesFile != null) config.plans().setInputPersonAttributeFile(inputDirectory + inputPersonAttributesFile);
 		scenario = ScenarioUtils.loadScenario(config);
 		
 		tagCarUsers();
@@ -109,8 +105,11 @@ public class TagCarAndSAVusers {
 		log.info("Car users: " + carUsers);
 		log.info("No car users: " + noCarUsers);
 		
-		new PopulationWriter(scenario.getPopulation()).write(outputPlans);
-		new ObjectAttributesXmlWriter(scenario.getPopulation().getPersonAttributes()).writeFile(outputPersonAttributes);
+		new PopulationWriter(scenario.getPopulation()).write(outputDirectory + outputPlansFile);
+		new ObjectAttributesXmlWriter(scenario.getPopulation().getPersonAttributes()).writeFile(outputDirectory + outputPersonAttributesFile);
+		
+		if (changeCar2TaxiTripsForAllPotentialSAVusers) new PopulationWriter(changeTripModes("person_potentialSAVuser", TransportMode.car, TransportMode.taxi)).write(outputDirectory + outputPlansFileChangeTripMode);;
+
 	}
 
 	private void tagPotentialSAVusers() {
@@ -230,5 +229,75 @@ public class TagCarAndSAVusers {
 		log.info("Tagging car users... Done.");
 	}
 
+	private Population changeTripModes(String subpopulationName, String fromMode, String toMode) {
+		log.info("Changing trip modes for subpopulation " + subpopulationName + ": " + fromMode + " --> " + toMode + "...");
+		final String[] attributes = {"CarOwnerInBaseCase"};
+
+		Scenario scenarioOutput = ScenarioUtils.loadScenario(ConfigUtils.createConfig());
+		Population populationOutput = scenarioOutput.getPopulation();
+		
+		for (Person person : scenario.getPopulation().getPersons().values()) {
+			
+			if (person.getPlans().size() > 1) throw new RuntimeException("More than one plan per person. Aborting...");
+			
+			PopulationFactory factory = populationOutput.getFactory();
+			Person personClone = factory.createPerson(person.getId());
+			for (String attribute : attributes) {
+				personClone.getAttributes().putAttribute(attribute, person.getAttributes().getAttribute(attribute));
+			}
+			populationOutput.addPerson(personClone);
+		
+			if (this.scenario.getPopulation().getPersonAttributes().getAttribute(person.getId().toString(), scenario.getConfig().plans().getSubpopulationAttributeName()).equals(subpopulationName)) {
+		
+				Plan plan = factory.createPlan();
+				
+				Leg previousLeg = null;
+				boolean previousActivityWasCarInteraction = false;
+				
+				for (PlanElement pE : person.getSelectedPlan().getPlanElements()) {
+					
+					if (pE instanceof Activity) {
+						Activity act = (Activity) pE;
+						if (act.getType().equals("car interaction")) {
+							// do not add the activity
+							previousActivityWasCarInteraction = true;
+						} else {
+							// add previous leg and current activity
+							if (previousLeg != null) plan.addLeg(previousLeg);
+							plan.addActivity(act);
+							
+							previousActivityWasCarInteraction = false;
+						}
+						
+					} else if (pE instanceof Leg) {
+						Leg leg = (Leg) pE;
+						if (previousActivityWasCarInteraction == false) {
+							previousLeg = leg;
+						} else {
+							previousLeg = null;
+						}
+						
+						if (leg.getMode().equals(TransportMode.car)) {
+							leg.setMode("taxi");
+							leg.setRoute(null);
+							leg.setTravelTime(Double.MIN_VALUE);
+							
+							plan.addLeg(leg);
+						}						
+					}	
+				}
+				// add modified plan
+				personClone.addPlan(plan);
+			} else {
+				// add unmodified plan
+				personClone.addPlan(person.getSelectedPlan());
+			}
+			
+		}		
+		log.info("Changing trip modes for subpopulation " + subpopulationName + ": " + fromMode + " --> " + toMode + "... Done.");
+		return populationOutput;
+
+	}
+	
 }
 
