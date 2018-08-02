f33fafc523c9ef23b5c0614fabaa92d5f1da90ce
sbraun <sbraun@FG16.vspwin.vsp.tu-berlin.de>
2018-08-02 16:58:51 +0200

    Revert "fixed counter, rule3Tester and Problem with Rule 4"
    This reverts commit 0717e8412f4292debc8ae67c5be918134bcad1b6.

--- tthunig/src/main/java/signals/gershenson/DgRoederGershensonSignalController.java
+++ tthunig/src/main/java/signals/gershenson/DgRoederGershensonSignalController.java
@@ -55,7 +55,7 @@ import java.util.Set;
  * (monitoredPlatoonTail) r=25m 
  * (minimumGREENtime) t_min = 4s 
  * (lengthOfPlatoonTails) m= 2 veh 
- * (threshold) n= 250 veh*s   (sbraun Feb.18)
+ * (threshold) n= 13.33 veh*s   (sbraun Feb.18)
  * 
  * @author dgrether, sbraun, droeder
  *
@@ -110,6 +110,34 @@ public class DgRoederGershensonSignalController implements SignalController {
 	private static final Logger log = Logger.getLogger(DgRoederGershensonSignalController.class);
 	
 	private SignalSystem system;
+
+	
+	//TODO fuer Setter in Config-Datei
+	//protected int tGreenMin =  0; // time in seconds
+	//protected int minCarsTime = 0; //
+	
+	//protected double storageCapacityOutlinkJam = 0.8;
+
+	//protected double interGreenTime = 0.; //TODO default should be at least 2
+	//protected double threshold = 13.33; //TODO should be higher
+	
+	//protected int lengthOfPlatoonTails = 2;
+	//protected int minimumGREENtime = 4;  //TODO should be higher maybe 20
+	//protected double monitoredPlatoonTail = 25.;
+	//protected double monitoredDistance = 50.;
+	//protected double minmumDistanceBehindIntersection =10.;
+	
+	//protected boolean outLinkJam;
+	//protected boolean maxRedTimeActive = false;
+	//protected double compGreenTime;
+	//protected double approachingRed;
+	//protected double approachingGreenLink;
+	//protected double approachingGreenLane;
+	//protected double carsOnRefLinkTime;
+	//protected boolean compGroupsGreen;
+	//protected SignalGroupState oldState;
+	
+	//protected double signalgrouptimecounter = 0.0;
 	private LinkSensorManager sensorManager;
 	private Scenario scenario;
 	private GershensonConfig gershensonConfig;
@@ -185,11 +213,9 @@ public class DgRoederGershensonSignalController implements SignalController {
 				}
 			}
 		}
-		//This is a fixed value and and doesn't have to be calculated in every iteration
+		
 		maximalNumberOfAgentsInDistance = (int)(gershensonConfig.getMonitoredDistance()/this.scenario.getConfig().jdeqSim().getCarSize());
 	}
-	
-	
 	private void addOutLinksWithoutBackLinkToMetadata(Link inLink, SignalGroupMetadata metadata){
 		for (Link outLink : inLink.getToNode().getOutLinks().values()){
 			if (!outLink.getToNode().equals(inLink.getToNode())){
@@ -230,8 +256,6 @@ public class DgRoederGershensonSignalController implements SignalController {
 				this.system.scheduleDropping(timeSeconds, group.getId());
 				timecounter.put(group.getId(), 0.);
 				counter.put(group.getId(), 0.);
-				//Ensures that if a Group is switched from GREEN to RED that it is not the active Group anymore
-				if(group.equals(activeSignalGroup)) activeSignalGroup=null;
 			}
 		}	
 	}
@@ -243,12 +267,14 @@ public class DgRoederGershensonSignalController implements SignalController {
 		if (!approachingVehiclesMap.containsKey(group.getId())) {
 			approachingVehiclesMap.put(group.getId(), new HashMap<Id<Link>,Integer>());
 		}
-		for (Link link : signalGroupIdMetadataMap.get(group.getId()).getInLinks()) {			
+		for (Link link : signalGroupIdMetadataMap.get(group.getId()).getInLinks()) {
+			//Integer cars = this.sensorManager.getNumberOfCarsOnLink(link.getId());
+			
+			//TODO Sensor-Manager doesn't seem to work right			
 			Integer cars = this.sensorManager.getNumberOfCarsInDistance(link.getId(), gershensonConfig.getMonitoredDistance(), now); 
 			approachingVehiclesMap.get(group.getId()).put(link.getId(), cars);
 		} 
 	}
-	
 	//method to monitor cars behind intersection
 	private void jamOnOutLink(SignalGroup group) {
 		if (!jammedOutLinkMap.containsKey(group.getId())) {
@@ -290,7 +316,7 @@ public class DgRoederGershensonSignalController implements SignalController {
 			int vehicles = 0;
 			for(Id<Link> link : approachingVehiclesMap.get(group.getId()).keySet()){
 				
-	//If in the distance there are more vehicles as physically possible, add the maximal critical number of vehicles to the counter
+				//Fix Agents distance here
 				if(maximalNumberOfAgentsInDistance < approachingVehiclesMap.get(group.getId()).get(link)){
 					vehicles += maximalNumberOfAgentsInDistance;
 				} else {
@@ -301,14 +327,17 @@ public class DgRoederGershensonSignalController implements SignalController {
 		}
 	}
 	
+	private int maximalNumberOfAgentsInDistance;
 	
 	
 	private void updatecounter(){
+		//counter.clear();		
 		for(SignalGroup group : this.system.getSignalGroups().values()) {
 			if (approachingVehiclesGroupMap.containsKey(group.getId())){
 				if (!counter.containsKey(group.getId())){
 					counter.put(group.getId(), 0.);
 				}
+					//counter.put(group.getId(), timecounter.get(group.getId())*approachingVehiclesGroupMap.get(group.getId()));
 				
 				if (group.getState() != null){
 					if (group.getState().equals((SignalGroupState.RED))){
@@ -317,17 +346,15 @@ public class DgRoederGershensonSignalController implements SignalController {
 				}
 			}
 		}
+
 	}
 	
-	private Boolean rule3Tester(Id<SignalGroup> signal, double time) {
+	private Boolean hasMoreThanMVehiclesInR(Id<SignalGroup> signal, double time) {
 		Boolean VehInR = false;
 		for (Link link : signalGroupIdMetadataMap.get(signal).getInLinks()) {
 			if(!VehInR) {
-				int numberVehiclesInShortDistance = sensorManager.getNumberOfCarsInDistance(link.getId(), gershensonConfig.getMonitoredPlatoonTail(), time);
-				if(numberVehiclesInShortDistance < gershensonConfig.getLengthOfPlatoonTails() && 
-						numberVehiclesInShortDistance > 0 &&
-						numberVehiclesInShortDistance - approachingVehiclesMap.get(signal).get(link.getId()) == 0
-						) {
+				if(sensorManager.getNumberOfCarsInDistance(link.getId(), gershensonConfig.getMonitoredPlatoonTail(), time) < gershensonConfig.getLengthOfPlatoonTails() && 
+						sensorManager.getNumberOfCarsInDistance(link.getId(), gershensonConfig.getMonitoredPlatoonTail(), time) > 0) {
 					VehInR = true;
 					log.info("There were "+sensorManager.getNumberOfCarsInDistance(link.getId(), gershensonConfig.getMonitoredPlatoonTail(), time) +" Vehicles in "+gershensonConfig.getMonitoredPlatoonTail() +"m in " + signal + " at time " +time + "s on link " + link.getId() );
 				}
@@ -344,6 +371,7 @@ public class DgRoederGershensonSignalController implements SignalController {
 				if(highest<counter.get(signalgroup)) highest = counter.get(signalgroup);
 			}
 		}
+		//if (highest == null) log.error("There are no Signalgroups in the, which are not jammed. Rule 6 should have triggerd before");
 		return highest;
 	}
 	
@@ -371,8 +399,6 @@ public class DgRoederGershensonSignalController implements SignalController {
 	private Map<Id<SignalGroup>, Double> counter = new HashMap<Id<SignalGroup>, Double>();	
 	private Map<Id<SignalGroup>, Boolean> jammedSignalGroup = new HashMap<Id<SignalGroup>,Boolean>();
 	private SignalGroup activeSignalGroup = null;
-	private int maximalNumberOfAgentsInDistance;
-	
 	
 	
 
@@ -382,9 +408,9 @@ public class DgRoederGershensonSignalController implements SignalController {
 	// Get data which is needed before going through the rule set	
 		
 		
-//	//This is just to demonstrate that the algorithm works
+//		//This is just to demonstrate that the algorithm works
 		if (timeSeconds%1==0) {
-	
+			//log.info("At "+timeSeconds +" "+ activeSignalGroup.getId() + " is active and is if not null");
 			if (activeSignalGroup!= null){
 				log.info("At "+timeSeconds +" "+ activeSignalGroup.getId().toString() + " is active and is" + activeSignalGroup.getState());
 			} else {
@@ -394,14 +420,10 @@ public class DgRoederGershensonSignalController implements SignalController {
 			log.info(" ");
 		}
 		
-		//add one second to the timecounter of eachgroup
-		//TODO adjust this so only the activegroup is counted to avoid this loop
 		for (SignalGroup group : this.system.getSignalGroups().values()){
 			timecounter.merge(group.getId(), 0., (a,b) -> a+1.);
 		}
 		
-		//Only run through the ruleset if there is no active Signalgroup (i.e. all are RED) or if the activeSignal-Group is Green
-		// -> If the active Signalgroup is Red it should be due to the intergreen time
 		if( activeSignalGroup ==null|| activeSignalGroup.getState().equals(SignalGroupState.GREEN)) {
 			
 			
@@ -412,19 +434,20 @@ public class DgRoederGershensonSignalController implements SignalController {
 			
 			
 			for (SignalGroup group : this.system.getSignalGroups().values()){
-		//timecounter.merge(group.getId(), 0., (a,b) -> a+1.);
-		//fills approachingVehiclesMap
+				//timecounter.merge(group.getId(), 0., (a,b) -> a+1.);
+				//fills approachingVehiclesMap
 				carsOnInLinks(group,timeSeconds);
-		//fills jammedOutLinkMap
+				//fills jammedOutLinkMap
 				jamOnOutLink(group);
 				
 				jammedSignalGroup.put(group.getId(), jammedafterIntersection(group));
 			}
 			
-		//fills approachingVehiclesGroupMap and counter (counter: Every second the number of vehicles in the monitored distance is added)
+			//log.error("At "+ timeSeconds + " are the following groups jammed: "+ jammedSignalGroup.toString());
+			//fills approachingVehiclesGroupMap and counter (product of internal times with vehicles)
 			carsApproachInGroup();
 			updatecounter();
-		//Find the group with the highest count given it has no outbound jam
+			//Find the group with the highest count (product of internal time with vehicles in D s.t. no OutLink-Jam)
 			Double signalGrouphighestCount = highestcount();
 			boolean thereAreVehiclesApproaching = vehiclesApproachingSomewhere();
 			
@@ -434,25 +457,26 @@ public class DgRoederGershensonSignalController implements SignalController {
 		//if the whole intersection is jammed OR
 		//no Cars approaching the SignalSystem.
 		//	-> turn all signals to RED	
-
+			
+		//TODO VehicleApproaching run in the beginning of method to avoid unnecessary loops
 			if (!jammedSignalGroup.containsValue(false)  || !thereAreVehiclesApproaching) {
 				//log.info("Envoke Rule 6 at "+timeSeconds + !vehiclesApproachingSomewhere());
 				log.info("Rule 6");
 				log.info("all jammed: "+ !jammedSignalGroup.containsValue(false));
 				log.info("no cars approaching: "+!thereAreVehiclesApproaching);
 				
-				//if (activeSignalGroup != null){
+				if (activeSignalGroup != null){
 					for (SignalGroup group : this.system.getSignalGroups().values()){ 			
-						if (activeSignalGroup != null && activeSignalGroup.equals(group)) switchlight(group,timeSeconds);
+						if (activeSignalGroup.equals(group)) switchlight(group,timeSeconds);
 					}
-					//activeSignalGroup = null;
-				//}
+					activeSignalGroup = null;
+				}
 				//if(!vehiclesApproachingSomewhere()) activeSignalGroup=null;
 			} else {
 		//Rule 5
 		//At least one Outlink is jammed - from all groups, which are not jammed turn the one with the highest counter to GREEN
 		//the others to RED
-		//The switchlightCounter ensures that the activeSignalGroup is set to null if necessary
+		//TODO implement a switchlight counter to ensure that activeSignal group is set to null if necessary
 				if (jammedSignalGroup.containsValue(true) && activeSignalGroup !=null) {
 					log.error("Envoke Rule 5 at "+timeSeconds + jammedSignalGroup.toString());
 					
@@ -468,21 +492,27 @@ public class DgRoederGershensonSignalController implements SignalController {
 							log.info("Fehler 1 - Rule 5");
 							log.info("active ");
 						} else {
-		// Set a not active Signalgroup to Green if its is not jammed and it has the highest number of cars approaching
-		// if at there are some
+						// Set a not active Signalgroup to Green if its is not jammed and it has the highest number of cars approaching
+						// if at there are some
 							if ( !jammedSignalGroup.get(group.getId()) && 
 									counter.get(group.getId())==signalGrouphighestCount && 
 									counter.get(group.getId())!=0. &&
-									group.getState().equals(SignalGroupState.RED) 				
+									group.getState().equals(SignalGroupState.RED) 
+									
 								|| (!activeSignalGroup.equals(group) && group.getState().equals(SignalGroupState.GREEN))){
-																
+									
+//									 && ) ||
+//									//(counter.get(group.getId())!=counter.get(signalGrouphighestCount)  && activeSignalGroup.equals(group)))
+//									(activeSignalGroup.equals(group)))
+//									/*signalGroupstatesMap.get(group.getId()).equals(SignalGroupState.GREEN)))*/ {
+								
 								switchlight(group,timeSeconds);
 								switchlightcounter++;
 								log.info("Fehler 2 - Rule 5 " + switchlightcounter);
 							}
 						}	
 					}
-		//If only one switch happened, all groups should be red and active group should be null
+					//If only one switch happened, all groups should be red and active group should be null
 					if (switchlightcounter==1) activeSignalGroup=null;
 					
 				} else {
@@ -527,10 +557,12 @@ public class DgRoederGershensonSignalController implements SignalController {
 		//Rule 3
 		//A signal should switch from GREEN to RED (according to Rule 1 and 2), but there are just a few cars left to pass: Don't switch
 							boolean hasfewerThanMVehiclesInR= false;
-							if (rule3Tester(activeSignalGroup.getId(), timeSeconds)	){
-								hasfewerThanMVehiclesInR = true;
-							}
-
+							if (hasMoreThanMVehiclesInR(activeSignalGroup.getId(), timeSeconds) ) hasfewerThanMVehiclesInR = true;
+							
+//							for (SignalGroup group : this.system.getSignalGroups().values()) {
+//								if (hasMoreThanMVehiclesInR(group.getId(), timeSeconds) ) counterRule3++;
+//								log.info("Rule 3");
+//							}
 							if (!hasfewerThanMVehiclesInR) {
 		//Rule 2
 		//A signal should switch from GREEN to RED (according to Rule 1), but internal time of the Group hasn't reached
@@ -555,7 +587,9 @@ public class DgRoederGershensonSignalController implements SignalController {
 											if(counter.get(groupR1.getId())<counter.get(group.getId())) groupR1=group;
 										}
 										
+										//log.info("Envoke Rule 1. Rule 2,3,4 are not violated. Switch " + groupR1.getId());
 										for (SignalGroup group : this.system.getSignalGroups().values()) {	
+											//if (group.getState().equals(SignalGroupState.GREEN)) switchlight(group,timeSeconds);
 											if (group.equals(activeSignalGroup)) switchlight(group,timeSeconds);
 											if (group.getState().equals(SignalGroupState.GREEN)) switchlight(group,timeSeconds);
 											if (group.equals(groupR1)) switchlight(group,timeSeconds);
@@ -569,7 +603,77 @@ public class DgRoederGershensonSignalController implements SignalController {
 			}
 		}
 	}
-
+//	// old code from daniel and dominik
+//	@Override
+//	public void updateState(double timeSeconds) {
+//		log.error("Signal system: " + this.system.getId() + " at time " + timeSeconds);
+//		//if the groups are switching do nothing
+//		if (this.isSwitching()){
+//			log.error("  Group: " + this.greenGroup.getId() + " is switching with state: " + this.greenGroup.getState());
+//			return;
+//		}
+//		log.error("  is not switching...");
+//		SignalGroup newGreenGroup = null;
+//		//rule 7 
+//		SignalGroup maxRedViolatingGroup = this.checkMaxRedTime(timeSeconds);
+//		if (maxRedViolatingGroup != null){
+//			newGreenGroup = maxRedViolatingGroup;
+//			log.error("new green group is : " + newGreenGroup.getId());
+//		}
+//		if (newGreenGroup != null){
+//			this.switchGroup2Red(timeSeconds, this.greenGroup);
+//			this.switchGroup2Green(timeSeconds, newGreenGroup);
+//		}
+//		//rule 6
+//		Set<SignalGroup> notOutLinkJamGroups = new HashSet<SignalGroup>();
+//		for (SignalGroup group : this.system.getSignalGroups().values()){
+//			if (!this.hasOutLinkJam(group, this.signalGroupIdMetadataMap.get(group.getId()))){
+//				notOutLinkJamGroups.add(group);
+//			}
+//		}
+//		if (this.greenGroup != null){
+//			if (!notOutLinkJamGroups.contains(this.greenGroup)){
+//				notOutLinkJamGroups.remove(this.greenGroup);
+//				this.switchGroup2Red(timeSeconds, this.greenGroup);
+//				this.greenGroup = null;
+//			}
+//		}
+//		if (this.greenGroup == null && !notOutLinkJamGroups.isEmpty()) { //we have an all red but one has no jam
+//			this.switchGroup2Green(timeSeconds, notOutLinkJamGroups.iterator().next());
+//		}
+//	}
+//		//rule 4
+//		
+//		for (SignalGroup group : this.system.getSignalGroups().values()){
+//			SignalGroupMetadata metadata = this.signalGroupIdMetadataMap.get(group.getId());
+//			//rule 6
+//			if (group.getState().equals(SignalGroupState.GREEN) && this.hasOutLinkJam(group, metadata)){ 
+//				//TODO schedule dropping
+//				continue;
+//			}
+//			
+//			else {
+//				//if all red switch to green
+//			
+//				//rule 4
+//				if (!approachingGreenInD() && approachingRedInD){
+//						//switch something
+//				}
+//				//rule 3
+//				if (group.getState().equals(SignalGroupState.GREEN) && hasMoreThanMVehiclesInR(group)){
+//					//don't switch
+//				}
+//				//rule 2
+//				if (group.getState().equals(SignalGroupState.GREEN) && greenTime(group) < minGreenTime){
+//					//don't switch
+//				}
+//				//rule 1
+//				if (group.getState().equals(SignalGroupState.RED) && numberCarsInD * timeRed > n_min){
+//					//switch to green
+//				}
+//			}
+//		}
+//	}
 			
 			
 	@Override
--- tthunig/src/main/java/signals/gershenson/GershensonConfig.java
+++ tthunig/src/main/java/signals/gershenson/GershensonConfig.java
@@ -3,12 +3,11 @@ package signals.gershenson;
 public class GershensonConfig {
 	private double storageCapacityOutlinkJam = 0.8;
 	//protected double maxRedTime = 15.0;
-	private double interGreenTime = 5.; 
-	//TODO this leads to reasonable results (try and error). Why did Gershenson think 13.33 was a good threshold?
-	//in the tests this led to inefficient switching after the minimum green-Time.
-	private double threshold = 250; 
+	private double interGreenTime = 5.; //TODO default should be at least 2
+	private double threshold = 250; //TODO should be higher
+	
 	private int lengthOfPlatoonTails = 2;
-	private double minimumGREENtime = 5;  //TODO must not be negative
+	private double minimumGREENtime = 5;  //TODO should be higher maybe 20
 	
 	//These are attributes for the Sensor
 	private double monitoredPlatoonTail = 25.;
@@ -26,13 +25,8 @@ public class GershensonConfig {
 	}
 	
 	public void setInterGreenTime(double interGreenTime){
-	
-	//Prevents negative intergreen times, if set by user set to 0	
-		if (interGreenTime < 0){
-			this.interGreenTime = 0.0;
-		} else this.interGreenTime = interGreenTime;
+		this.interGreenTime = interGreenTime;
 	}
-	
 	public double getInterGreenTime() {
 		return interGreenTime;
 	}
