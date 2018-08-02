6ff6a5c9569d1f0b16eefad60ee96470e7e7b5f4
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-08-02 20:30:41 +0200

    adding travel disutilities for dvrp

---
+++ ikaddoura/src/main/java/playground/ikaddoura/optAV/DvrpMoneyTimeDistanceTravelDisutility.java
@@ -0,0 +1,132 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2015 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.ikaddoura.optAV;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.contrib.noise.personLinkMoneyEvents.PersonLinkMoneyEvent;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.vehicles.Vehicle;
+
+import playground.ikaddoura.moneyTravelDisutility.MoneyEventAnalysis;
+import playground.ikaddoura.moneyTravelDisutility.data.AgentFilter;
+import playground.ikaddoura.moneyTravelDisutility.data.LinkInfo;
+import playground.ikaddoura.moneyTravelDisutility.data.TimeBin;
+
+/**
+* 
+* A travel disutility which adds a term for monetary payments to the disutility which is passed into the constructor.
+* The monetary payments are estimated based on time, link- and agent-specific {@link PersonLinkMoneyEvent}s in the previous iteration(s)
+* that are caught and analyzed by {@link MoneyEventAnalysis}.
+* 
+* @author ikaddoura
+*/
+
+public class DvrpMoneyTimeDistanceTravelDisutility implements TravelDisutility {
+
+	private final double timeBinSize;
+	private MoneyEventAnalysis moneyEventAnalysis;
+	private AgentFilter vehicleFilter;
+	private final double costPerSec;
+	private final TravelTime travelTime;
+	
+	public DvrpMoneyTimeDistanceTravelDisutility(
+			TravelTime travelTime,
+			double costPerSec, 
+			int timeBinSize,
+			MoneyEventAnalysis moneyAnalysis,
+			AgentFilter vehicleFilter) {
+		
+		this.travelTime = travelTime;
+		this.timeBinSize = timeBinSize;
+		this.costPerSec = costPerSec;
+		this.vehicleFilter = vehicleFilter;
+		this.moneyEventAnalysis = moneyAnalysis;	
+	}
+
+	@Override
+	public double getLinkTravelDisutility(Link link, double time, Person person, Vehicle vehicle) {
+		
+		double travelDisutility = 0.;
+		
+		if (travelTime != null) {
+			travelDisutility = costPerSec * travelTime.getLinkTravelTime(link, time, person, vehicle);
+		} else {
+			travelDisutility = costPerSec * (link.getLength() / link.getFreespeed());
+		}
+		double linkExpectedTollDisutility = calculateExpectedTollDisutility(link, time, person, vehicle);
+				
+		return travelDisutility + linkExpectedTollDisutility;
+	}
+
+	@Override
+	public double getLinkMinimumTravelDisutility(Link link) {
+		return costPerSec * (link.getLength() / link.getFreespeed());
+	}
+
+	private double calculateExpectedTollDisutility(final Link link, final double time, final Person person, final Vehicle vehicle) {
+				
+		/* The following is an estimate of the tolls that an agent would have to pay if choosing that link in the next
+		iteration i based on the tolls in iteration i-1 */
+				
+		int intervalNr = getIntervalNr(time);
+		
+		double estimatedAmount = 0.;
+
+		LinkInfo linkInfo = moneyEventAnalysis.getLinkId2info().get(link.getId());
+		if (linkInfo != null) {
+						
+			TimeBin timeBin = linkInfo.getTimeBinNr2timeBin().get(intervalNr);
+			if (timeBin != null) {
+
+				if(this.vehicleFilter != null) {
+					Id<Person> personId = null;
+					if (person != null) {
+						personId = person.getId();
+					} else {
+						// person Id is null
+					}
+					String agentType = vehicleFilter.getAgentTypeFromId(personId);
+					Double avgMoneyAmountVehicleType = timeBin.getAgentTypeId2avgAmount().get(agentType);
+					
+					if (avgMoneyAmountVehicleType != null && avgMoneyAmountVehicleType != 0.) {
+						estimatedAmount = avgMoneyAmountVehicleType;
+					} else {
+						estimatedAmount = timeBin.getAverageAmount();
+					}
+				} else {
+					estimatedAmount = timeBin.getAverageAmount();
+				}
+			}
+		}
+				
+		double linkExpectedTollDisutility = -1 * estimatedAmount;
+		return linkExpectedTollDisutility;
+	}
+	
+	private int getIntervalNr(double time) {
+		int timeBinNr = (int) (time / timeBinSize);
+		return timeBinNr;
+	}
+	
+}
+
