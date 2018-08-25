23a039bd429dc146e91e0171449044dc9362b1a2
dziemke <ziemke@vsp.tu-berlin.de>
2018-08-25 09:07:58 +0200

    tidy up berlin accessibility runner

--- dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationBerlinV3.java
+++ dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationBerlinV3.java
@@ -4,14 +4,12 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup;
-import org.matsim.contrib.accessibility.AccessibilityModule;
-import org.matsim.contrib.accessibility.FacilityTypes;
-import org.matsim.contrib.accessibility.Modes4Accessibility;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup.AreaOfAccesssibilityComputation;
+import org.matsim.contrib.accessibility.AccessibilityModule;
+import org.matsim.contrib.accessibility.Modes4Accessibility;
 import org.matsim.contrib.accessibility.utils.AccessibilityUtils;
 import org.matsim.contrib.accessibility.utils.VisualizationUtils;
 import org.matsim.core.config.Config;
@@ -25,56 +23,34 @@ import org.matsim.facilities.ActivityFacilities;
 
 import com.vividsolutions.jts.geom.Envelope;
 
-import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
-
+/**
+ * Compute accessibility based on Berlin scenario version 5.0
+ * 
+ * @author dziemke
+ */
 public class AccessibilityComputationBerlinV3 {
-	private static final Logger log = Logger.getLogger(AccessibilityComputationBerlinV3.class);
 	
 	public static void main(String[] args) {
-		
-		Config config;
-		
-		if ( args.length==0 || args[0]=="" ) {
-//			String configFile = "scenarios/berlin-v5.0-0.1pct-2018-06-18/input/berlin-5.0_config_reduced.xml";
-//			String configFile = "../../matsim-berlin/scenarios/berlin-v5.0-0.1pct-2018-06-18/input/berlin-5.0_config_reduced.xml";
-			String configFile = "../../matsim-berlin/scenarios/berlin-v5.0-1pct-2018-06-18/input/berlin-5.0_config.xml";
-			log.info("config file: " + configFile);
-			config = ConfigUtils.loadConfig(configFile);
-			
-		} else {
-			String configFile = args[0];
-			log.info("config file: " + configFile);
-			config = ConfigUtils.loadConfig(configFile);
-		}
-		
-//		config.controler().setOverwriteFileSetting(OverwriteFileSetting.failIfDirectoryExists);
-		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
-		
-		// Acc
-//		config.controler().setOutputDirectory("./data/output/berlin-5.0_1/002_acc_car/");
-		config.controler().setOutputDirectory("../../shared-svn/projects/accessibility_berlin/output/v3/500_pt_shop/");
-		// End acc
-		
-//		RunBerlinScenario runBerlinScenario = new RunBerlinScenario();
-		AccessibilityComputationBerlinV3 runBerlinScenario = new AccessibilityComputationBerlinV3();
-		runBerlinScenario.run(config);
-	}
+		Config config = ConfigUtils.loadConfig("../../matsim-berlin/scenarios/berlin-v5.0-1pct-2018-06-18/input/berlin-5.0_config.xml");
 
-	void run(Config config) {
+		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+		config.controler().setOutputDirectory("../../shared-svn/projects/accessibility_berlin/output/v3/500_car_edu_16/");
+
+		config.facilities().setInputFile(new File("../../shared-svn/projects/accessibility_berlin/osm/berlin/amenities/2018-05-30/facilities.xml").getAbsolutePath());
 		
 		config.subtourModeChoice().setProbaForRandomSingleTripMode(0.5);
+
+		config.transitRouter().setCacheTree(true);
 		
-		// Acc
-		config.facilities().setInputFile(new File("../../shared-svn/projects/accessibility_berlin/osm/berlin/amenities/2018-05-30/facilities.xml").getAbsolutePath());
-		// End acc
-		
-		// ---
+		config.plansCalcRoute().setInsertingAccessEgressWalk(true);
+		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+		config.qsim().setTrafficDynamics(TrafficDynamics.kinematicWaves);
+
+		ConfigUtils.setVspDefaults(config);
 		
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 
-		// ---
-		
-		// Acc
+		// Accessibility configurations
 		Double cellSize = 500.;
 		boolean push2Geoserver = false; // Set true for run on server
 		boolean createQGisOutput = true; // Set false for run on server
@@ -83,55 +59,26 @@ public class AccessibilityComputationBerlinV3 {
 		String scenarioCRS = "EPSG:31468"; // EPSG:31468 = DHDN GK4
 		
 		AccessibilityConfigGroup acg = ConfigUtils.addOrGetModule(config, AccessibilityConfigGroup.class);
-//		acg.setAreaOfAccessibilityComputation(AreaOfAccesssibilityComputation.fromBoundingBox);
+		acg.setTimeOfDay(16*60.*60.);
 		acg.setAreaOfAccessibilityComputation(AreaOfAccesssibilityComputation.fromShapeFile);
 		acg.setShapeFileCellBasedAccessibility("../../shared-svn/studies/countries/de/open_berlin_scenario/input/shapefiles/2013/Berlin_DHDN_GK4.shp");
 		acg.setEnvelope(envelope);
 		acg.setCellSizeCellBasedAccessibility(cellSize.intValue());
 //		acg.setComputingAccessibilityForMode(Modes4Accessibility.walk, true);
 		acg.setComputingAccessibilityForMode(Modes4Accessibility.freespeed, false);
-//		acg.setComputingAccessibilityForMode(Modes4Accessibility.car, true);
-		acg.setComputingAccessibilityForMode(Modes4Accessibility.pt, true);
+		acg.setComputingAccessibilityForMode(Modes4Accessibility.car, true);
+//		acg.setComputingAccessibilityForMode(Modes4Accessibility.pt, true);
 		acg.setOutputCrs(scenarioCRS);
-		
-		ConfigUtils.setVspDefaults(config);
-		
-//		config.plansCalcRoute().setInsertingAccessEgressWalk(true);
-		
-		// ---------- Schedule-based pt
-//		config.transit().setUseTransit(true);
-//		config.transit().setTransitScheduleFile("../../runs-svn/open_berlin_scenario/b5_22e1a/b5_22e1a.output_transitSchedule.xml.gz");
-//		config.transit().setVehiclesFile("../../runs-svn/open_berlin_scenario/b5_22e1a/b5_22e1a.output_transitVehicles.xml.gz");
-//		config.qsim().setEndTime(100*3600.);
-//		
-		config.transitRouter().setCacheTree(true);
-//		
-//		ModeParams ptParams = new ModeParams(TransportMode.transit_walk);
-//		config.planCalcScore().addModeParams(ptParams);
-		
-//		Scenario scenario2 = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-//		new MatsimNetworkReader(TransformationFactory.getCoordinateTransformation("EPSG:4326", scenarioCRS), 
-//		scenario2.getNetwork()).readFile("../../../shared-svn/projects/maxess/data/nairobi/digital_matatus/matsim_2015-06-16_2/network.xml");
-//		MergeNetworks.merge(network, null, scenario2.getNetwork());
-
-		// ... trying to alter settings to drive down the number of "35527609 transfer links to be added".
-//		config.transitRouter().setAdditionalTransferTime(additionalTransferTime); // default: 0.0
-//		config.transitRouter().setDirectWalkFactor(directWalkFactor); // default: 1.0
-//		config.transitRouter().setMaxBeelineWalkConnectionDistance(0.); // default: 100.0
-//		config.transitRouter().setExtensionRadius(0.); // default: 200.0
-//		config.transitRouter().setSearchRadius(0.); // default: 1000.0
-		// ----------
 		
 //		final List<String> activityTypes = Arrays.asList(new String[]{FacilityTypes.EDUCATION, "s"});
 //		final List<String> activityTypes = Arrays.asList(new String[]{FacilityTypes.EDUCATION});
 		final List<String> activityTypes = Arrays.asList(new String[]{"s"});
 		
 		final ActivityFacilities densityFacilities = AccessibilityUtils.createFacilityForEachLink(scenario.getNetwork()); // will be aggregated in downstream code!
-		// End acc
 		
 		Controler controler = new Controler(scenario);
 		
-		// use the sbb pt raptor router
+		// Use sbb pt raptor router
 //		controler.addOverridingModule(new AbstractModule() {
 //			@Override
 //			public void install() {
@@ -139,7 +86,7 @@ public class AccessibilityComputationBerlinV3 {
 //			}
 //		});
 		
-		// use the (congested) car travel time for the teleported ride mode
+		// Use the (congested) car travel time for the teleported ride mode
 		controler.addOverridingModule(new AbstractModule(){
 			@Override
 			public void install() {
@@ -147,7 +94,7 @@ public class AccessibilityComputationBerlinV3 {
 				addTravelDisutilityFactoryBinding(TransportMode.ride).to(carTravelDisutilityFactoryKey());        }
 	    });
 		
-		// Acc
+		// Accessibility module
 		for (String activityType : activityTypes) {
 			AccessibilityModule module = new AccessibilityModule();
 			module.setConsideredActivityType(activityType);
@@ -155,18 +102,11 @@ public class AccessibilityComputationBerlinV3 {
 			module.setPushing2Geoserver(push2Geoserver);
 			controler.addOverridingModule(module);
 		}
-		// End acc
-		
-		// vsp defaults
-		config.plansCalcRoute().setInsertingAccessEgressWalk(true);
-		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
-		config.qsim().setTrafficDynamics(TrafficDynamics.kinematicWaves);
-		
 		controler.run();
 		
-		// Acc
+		// Accessibility visualizations in QGis
 		if (createQGisOutput) {
-			final boolean includeDensityLayer = false;
+			final boolean includeDensityLayer = true;
 			final Integer range = 9; // In the current implementation, this must always be 9
 			final Double lowerBound = -3.5; // (upperBound - lowerBound) ideally nicely divisible by (range - 2)
 			final Double upperBound = 3.5;
@@ -183,8 +123,5 @@ public class AccessibilityComputationBerlinV3 {
 				}
 			}
 		}
-		// End acc
-	
-		log.info("Done.");
 	}
 }
