ba364e6d273c45a7b2e54a91ee589c7b5b82a9ca
kainagel <nagel@vsp.tu-berlin.de>
2018-09-06 18:02:54 +0200

    remove unused (and no longer existing) import

--- vspshk/src/main/java/playground/fzwick/RunExample.java
+++ vspshk/src/main/java/playground/fzwick/RunExample.java
@@ -11,7 +11,6 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.network.NetworkUtils;
-import org.matsim.core.network.io.NetworkReaderMatsimV1;
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunExample {
