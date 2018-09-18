d88a50d01987d0b2e69d90acae1ecc15b1badcc1
Michal Maciejewski <michal.mac@gmail.com>
2018-09-18 14:05:54 +0200

    change return of DrtRequestValidator from boolean to set of violated constraints (empty set means successful validation)

--- jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/saturdaynight/ZonalBasedRequestValidator.java
+++ jbischoff/src/main/java/playground/jbischoff/sharedTaxiBerlin/saturdaynight/ZonalBasedRequestValidator.java
@@ -18,87 +18,102 @@
  * *********************************************************************** */
 
 /**
- * 
+ *
  */
 package playground.jbischoff.sharedTaxiBerlin.saturdaynight;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.inject.Inject;
 
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.network.*;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.drt.data.DrtRequest;
-import org.matsim.contrib.drt.data.validator.*;
+import org.matsim.contrib.drt.data.validator.DefaultDrtRequestValidator;
+import org.matsim.contrib.drt.data.validator.DrtRequestValidator;
 import org.matsim.core.utils.geometry.geotools.MGC;
 
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
 
 /**
- * @author  jbischoff
- *
+ * @author jbischoff
  */
+
 /**
  *
  */
 public class ZonalBasedRequestValidator implements DrtRequestValidator {
-	
-	private Map<String,Geometry> zones ;
-	private Map<Id<Link>,String> link2zone = new HashMap<>();
+
+	private Map<String, Geometry> zones;
+	private Map<Id<Link>, String> link2zone = new HashMap<>();
 	private Network network;
 	DefaultDrtRequestValidator delegate = new DefaultDrtRequestValidator();
+
 	/**
-	 * 
+	 *
 	 */
 	@Inject
 	public ZonalBasedRequestValidator(Network network, ZonalSystem initialZones) {
 		this.zones = initialZones.getZones();
 		this.network = network;
-		
+
 	}
-	
-	
+
 	/* (non-Javadoc)
 	 * @see org.matsim.contrib.drt.data.validator.DrtRequestValidator#validateDrtRequest(org.matsim.contrib.drt.data.DrtRequest)
 	 */
 	@Override
-	public boolean validateDrtRequest(DrtRequest request) {
-		return delegate.validateDrtRequest(request) && //
-				linkIsCovered(request.getFromLink()) && linkIsCovered(request.getToLink());
+	public Set<String> validateDrtRequest(DrtRequest request) {
+		//fail-fast ==> return the first encountered cause
+		Set<String> causes = delegate.validateDrtRequest(request);
+		if (!causes.isEmpty()) {
+			return causes;
+		}
+		if (!linkIsCovered(request.getFromLink())) {
+			return Collections.singleton("fromLink not covered");
+		}
+		if (!linkIsCovered(request.getToLink())) {
+			return Collections.singleton("toLink not covered");
+		}
+		return causes;//==empty set
 	}
 
-	
-	public void updateZones(Map<String,Geometry> newZones ){
+	public void updateZones(Map<String, Geometry> newZones) {
 		this.zones = newZones;
 		this.link2zone.clear();
 	}
-	
-	
-	private boolean linkIsCovered(Link l){
-		if (getZoneForLinkId(l.getId())!=null) 
+
+	private boolean linkIsCovered(Link l) {
+		if (getZoneForLinkId(l.getId()) != null)
 			return true;
-		else return false;
-	}	
-	
-	private String getZoneForLinkId(Id<Link> linkId){
-		if (this.link2zone.containsKey(linkId)){
+		else
+			return false;
+	}
+
+	private String getZoneForLinkId(Id<Link> linkId) {
+		if (this.link2zone.containsKey(linkId)) {
 			return link2zone.get(linkId);
 		}
-		
+
 		Point linkCoord = MGC.coord2Point(network.getLinks().get(linkId).getCoord());
-		
-		for (Entry<String, Geometry> e : zones.entrySet()){
-			if (e.getValue().contains(linkCoord)){
+
+		for (Entry<String, Geometry> e : zones.entrySet()) {
+			if (e.getValue().contains(linkCoord)) {
 				link2zone.put(linkId, e.getKey());
 				return e.getKey();
 			}
 		}
 		link2zone.put(linkId, null);
 		return null;
-		
+
 	}
-	
+
 	/**
 	 * @return the zones
 	 */
