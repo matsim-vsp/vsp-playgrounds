056f73ede6ffbf1aee8418b9a9e6157163a0e6cb
Michal Maciejewski <michal.mac@gmail.com>
2018-09-18 17:55:01 +0200

    add DrtRequestRejectedEvent.cause to store rejection causes

--- jbischoff/src/main/java/playground/jbischoff/drt/cottbus/analysis/DrtEventsReader.java
+++ jbischoff/src/main/java/playground/jbischoff/drt/cottbus/analysis/DrtEventsReader.java
@@ -85,9 +85,10 @@ public class DrtEventsReader extends MatsimXmlParser {
                 Map<String, String> attributes = event.getAttributes();
 
                 Double time = Double.parseDouble(attributes.get(DrtRequestRejectedEvent.ATTRIBUTE_TIME));
-                Id<Request> requestId = Id.create(attributes.get(DrtRequestSubmittedEvent.ATTRIBUTE_REQUEST), Request.class);
+                Id<Request> requestId = Id.create(attributes.get(DrtRequestRejectedEvent.ATTRIBUTE_REQUEST), Request.class);
+                String cause = attributes.get(DrtRequestRejectedEvent.ATTRIBUTE_CAUSE);
 
-                return new DrtRequestRejectedEvent(time, requestId);
+                return new DrtRequestRejectedEvent(time, requestId, cause);
             }
         };
 
