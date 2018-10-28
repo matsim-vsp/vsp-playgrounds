20b29dbf9f878581f582223fbf27cc94e99ef6b8
dziemke <ziemke@vsp.tu-berlin.de>
2018-10-28 12:19:59 +0100

    other paths

--- dziemke/src/main/java/playground/dziemke/utils/DZPlanFileModifier.java
+++ dziemke/src/main/java/playground/dziemke/utils/DZPlanFileModifier.java
@@ -38,19 +38,22 @@ public class DZPlanFileModifier {
 //		String inputPlansFile = "../../runs-svn/open_berlin_scenario/b5_22/b5_22.output_plans.xml.gz";
 //		String outputPlansFile = "../../runs-svn/open_berlin_scenario/b5_22/b5_22.output_plans_no_links.xml.gz";
 		
-		String inputPlansFile = "../../imob/data/scenarios/limburg_frac2/matsim/population/population_50pct_no-children.xml.gz";
-		String outputPlansFile = "../../imob/data/scenarios/limburg_frac2/matsim/population/population_10pct_no-children.xml.gz";
+//		String inputPlansFile = "../../imob/data/scenarios/limburg_frac2/matsim/population/population_50pct_no-children.xml.gz";
+//		String outputPlansFile = "../../imob/data/scenarios/limburg_frac2/matsim/population/population_10pct_no-children.xml.gz";
+		
+		String inputPlansFile = "../../shared-svn/studies/countries/de/open_berlin_scenario/be_6/population/population_census.xml.gz";
+		String outputPlansFile = "../../shared-svn/studies/countries/de/open_berlin_scenario/be_6/population/population_1pct.xml.gz";
 		
 //		String inputPlansFile = "../../shared-svn/studies/countries/de/open_berlin_scenario/be_5/cemdap_input/502/plans1.xml.gz";
 //		String outputPlansFile = "../../shared-svn/studies/countries/de/open_berlin_scenario/be_6/population/plans_10000.xml.gz";
-		double selectionProbability = 0.2;
-		boolean onlyTransferSelectedPlan = true;
-//		boolean onlyTransferSelectedPlan = false;
+//		double selectionProbability = 0.2;
+		double selectionProbability = 0.01;
+//		boolean onlyTransferSelectedPlan = true;
+		boolean onlyTransferSelectedPlan = false;
 		boolean considerHomeStayingAgents = true;
 		boolean includeNonSelectedStayHomePlans = true;
 		boolean onlyConsiderPeopleAlwaysGoingByCar = false;
-//		int maxNumberOfAgentsConsidered = 10000;
-		int maxNumberOfAgentsConsidered = 1000000;
+		int maxNumberOfAgentsConsidered = 10000000;
 
 		boolean removeLinksAndRoutes = false;
 //		boolean removeLinksAndRoutes = true;
