239071f6267029e0a10b741ccfb5a0c110f34e33
ikaddoura <kaddoura@vsp.tu-berlin.de>
2018-11-05 16:11:46 +0100

    minor

--- ikaddoura/src/main/java/playground/ikaddoura/analysis/IKAnalysisRun.java
+++ ikaddoura/src/main/java/playground/ikaddoura/analysis/IKAnalysisRun.java
@@ -117,7 +117,7 @@ public class IKAnalysisRun {
 	
 	private final List<String> modes;
 
-	private String outputDirectoryName = "analysis-ik-v1.3";
+	private String outputDirectoryName = "analysis-ik-v1.4";
 
 	private final String visualizationScriptInputDirectory;
 
