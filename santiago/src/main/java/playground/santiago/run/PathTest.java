f95a921dbb676a9d39d875868aebadeca110b83d
LeoCamus <leocamusc@gmail.com>
2018-11-18 23:47:59 -0300

    Checking for paths in new OS

--- santiago/src/main/java/playground/santiago/run/PathTest.java
+++ santiago/src/main/java/playground/santiago/run/PathTest.java
@@ -1,22 +1,14 @@
 package playground.santiago.run;
 
-import java.io.BufferedWriter;
 
-import org.matsim.core.utils.io.IOUtils;
+import java.io.File;
 
 public class PathTest {
 
 	public static void main(String[] args) {
-		String pathToTest = "../../runs-svn/santiago/";
-		String nameTestFile = "testingPaths.txt";
-		String outputFile = pathToTest + nameTestFile;
-		
-		try (BufferedWriter writer = IOUtils.getBufferedWriter(outputFile)) {
-			writer.write("This is a test");
-			writer.close();
-		} catch (Exception e) {
-			throw new RuntimeException("Data is not written. Reason "+e );
-		}
+		File file = new File("Hola");
+		String path = file.getAbsolutePath();
+		System.out.println(path);
 
 	}
 
