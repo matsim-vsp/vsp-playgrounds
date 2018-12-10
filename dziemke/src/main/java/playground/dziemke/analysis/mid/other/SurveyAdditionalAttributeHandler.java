d0fd98b55579e2e38a6f9d692d22a1792b36e5fc
gabrielthunig <gabriel.thunig@web.de>
2018-12-10 14:22:06 +0100

    move too own package

---
+++ dziemke/src/main/java/playground/dziemke/analysis/mid/other/SurveyAdditionalAttributeHandler.java
@@ -0,0 +1,17 @@
+package playground.dziemke.analysis.mid.other;
+
+import org.matsim.api.core.v01.population.Leg;
+
+public class SurveyAdditionalAttributeHandler {
+    //PersonSurveyAttributeHandler
+
+    //population
+    public static final String SURVEY = "survey";
+    public static final String MID = "mid";
+    public static final String SRV = "srv";
+
+    //leg
+    public static final String DISTANCE_BEELINE_M = "distanceBeeline_m";
+    public static final String SPEED_M_S = "speed_m_s";
+
+}
