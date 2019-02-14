be10296f66f34a545d43f90cb57070fd19dbcdf7
kainagel <nagel@vsp.tu-berlin.de>
2019-02-14 20:00:47 +0100

    throw exception where api is no longer public

--- dgrether/src/main/java/playground/dgrether/analysis/charts/DgTravelTimeCalculatorChart.java
+++ dgrether/src/main/java/playground/dgrether/analysis/charts/DgTravelTimeCalculatorChart.java
@@ -48,122 +48,127 @@ import playground.dgrether.analysis.charts.utils.DgColorScheme;
  */
 public class DgTravelTimeCalculatorChart implements DgChart {
 
-  private static final Logger log = Logger
-      .getLogger(DgTravelTimeCalculatorChart.class);
-  
-  private TravelTimeCalculator calculator;
+	private static final Logger log = Logger
+								.getLogger(DgTravelTimeCalculatorChart.class);
 
-  private Map<List<Id<Link>>, XYSeries> linkIds = new HashMap<>();
+	private TravelTimeCalculator calculator;
 
-  private double endTime = Double.POSITIVE_INFINITY;
+	private Map<List<Id<Link>>, XYSeries> linkIds = new HashMap<>();
 
-  private double startTime = 0;
-  
-  
-  public DgTravelTimeCalculatorChart(TravelTimeCalculator travelTimeCalculator) {
-    this.calculator = travelTimeCalculator;
-  }
+	private double endTime = Double.POSITIVE_INFINITY;
 
-  public void addLinkId(List<Id<Link>> ids) {
-    StringBuilder title = new StringBuilder();
-    title.append("Link");
-    for (Id<Link> id : ids) {
-      title.append(" ");
-      title.append(id);
-    }
-    this.linkIds.put(ids, new XYSeries(title.toString(), false, false));
-  }
-  
-  private XYSeriesCollection createDataSet(){
-    XYSeriesCollection dataset = new XYSeriesCollection();
-    
-    int numSlots = this.calculator.getNumSlots();
-    int binSize = this.calculator.getTimeSlice();
-    int maxtime = numSlots * binSize;
-    
-    double startSecond = startTime;
-    double endSecond;
-    if (maxtime < endTime) {
-      endSecond = maxtime;
-    }
-    else {
-      endSecond = endTime;
-    }
-    
-    
-    double tt;
-    XYSeries series;
-    for (Entry<List<Id<Link>>, XYSeries> e : this.linkIds.entrySet()){
-      series = e.getValue();
-      dataset.addSeries(series);
-//      log.error("link: "+ e.getKey());
-      for (double i = startSecond; i < endSecond; i++) {
-        tt = 0;
-        for (Id<Link> id : e.getKey()){
-//          tt += this.calculator.getLinkTravelTime(id, i);
-        	throw new RuntimeException("You need the full link instead of just the id here! See TravelTimeCalculator.getLinkTravelTime()... ");
-        }
-//        log.error("time: " + i + " tt " + tt);
-        e.getValue().add(i, tt);
-      }
-    }
-    
-    return dataset;
-  }
-  
+	private double startTime = 0;
 
-  @Override
+
+	public DgTravelTimeCalculatorChart(TravelTimeCalculator travelTimeCalculator) {
+		this.calculator = travelTimeCalculator;
+	}
+
+	public void addLinkId(List<Id<Link>> ids) {
+		StringBuilder title = new StringBuilder();
+		title.append("Link");
+		for (Id<Link> id : ids) {
+			title.append(" ");
+			title.append(id);
+		}
+		this.linkIds.put(ids, new XYSeries(title.toString(), false, false));
+	}
+
+	private XYSeriesCollection createDataSet(){
+		XYSeriesCollection dataset = new XYSeriesCollection();
+
+//		int numSlots = this.calculator.getNumSlots();
+//		int binSize = this.calculator.getTimeSlice();
+		int numSlots = -1 ;
+		int binSize = -1 ;
+		if ( true ) {
+			throw new RuntimeException( "no longer supported (internals of travel time calculator publicly exposed)" ) ;
+		}
+		int maxtime = numSlots * binSize;
+
+		double startSecond = startTime;
+		double endSecond;
+		if (maxtime < endTime) {
+			endSecond = maxtime;
+		}
+		else {
+			endSecond = endTime;
+		}
+
+
+		double tt;
+		XYSeries series;
+		for (Entry<List<Id<Link>>, XYSeries> e : this.linkIds.entrySet()){
+			series = e.getValue();
+			dataset.addSeries(series);
+			//      log.error("link: "+ e.getKey());
+			for (double i = startSecond; i < endSecond; i++) {
+				tt = 0;
+				for (Id<Link> id : e.getKey()){
+					//          tt += this.calculator.getLinkTravelTime(id, i);
+					throw new RuntimeException("You need the full link instead of just the id here! See TravelTimeCalculator.getLinkTravelTime()... ");
+				}
+				//        log.error("time: " + i + " tt " + tt);
+				e.getValue().add(i, tt);
+			}
+		}
+
+		return dataset;
+	}
+
+
+	@Override
 	public JFreeChart createChart() {
-    XYSeriesCollection dataset = this.createDataSet();
-    XYPlot plot = new XYPlot();
-    DgAxisBuilder axisBuilder = new DgDefaultAxisBuilder();
-    ValueAxis xAxis = axisBuilder.createValueAxis("Simulation Time");
-//    xAxis.setRange(this.controllerConfig.getFirstIteration(), this.controllerConfig.getLastIteration() + 2);
-    ValueAxis yAxis = axisBuilder.createValueAxis("Travel Time");
-//    yAxis.setRange(-0.05, 0.3);
-//    xAxis.setVisible(false);
-//    xAxis.setFixedAutoRange(1.0);
-    plot.setDomainAxis(xAxis);
-    plot.setRangeAxis(yAxis);
-    
-    DgColorScheme colorScheme = new DgColorScheme();
-    
-    XYItemRenderer renderer2;
-    renderer2 = new XYLineAndShapeRenderer(true, false);
-    renderer2.setSeriesItemLabelsVisible(0, true);
-//    renderer2.setSeriesItemLabelGenerator(0, this.labelGenerator);
-    plot.setDataset(0, dataset);
-    renderer2.setSeriesStroke(0, new BasicStroke(1.0f));
-    renderer2.setSeriesOutlineStroke(0, new BasicStroke(1.0f));
-    renderer2.setSeriesPaint(0, colorScheme.getColor(1, "a"));
-    renderer2.setSeriesStroke(1, new BasicStroke(1.0f));
-    renderer2.setSeriesOutlineStroke(1, new BasicStroke(1.0f));
-    renderer2.setSeriesPaint(1, colorScheme.getColor(2, "a"));
-    renderer2.setSeriesStroke(2, new BasicStroke(1.0f));
-    renderer2.setSeriesOutlineStroke(2, new BasicStroke(1.0f));
-    renderer2.setSeriesPaint(2, colorScheme.getColor(3, "a"));
-    renderer2.setSeriesStroke(3, new BasicStroke(1.0f));
-    renderer2.setSeriesOutlineStroke(3, new BasicStroke(1.0f));
-    renderer2.setSeriesPaint(3, colorScheme.getColor(4, "a"));
-    
-    plot.setRenderer(0, renderer2);
-    
-    JFreeChart chart = new JFreeChart("", plot);
-    chart.setBackgroundPaint(ChartColor.WHITE);
-    chart.getLegend().setItemFont(axisBuilder.getAxisFont());
-    chart.setTextAntiAlias(true);
-//    chart.removeLegend();
-    return chart;
-  }
+		XYSeriesCollection dataset = this.createDataSet();
+		XYPlot plot = new XYPlot();
+		DgAxisBuilder axisBuilder = new DgDefaultAxisBuilder();
+		ValueAxis xAxis = axisBuilder.createValueAxis("Simulation Time");
+		//    xAxis.setRange(this.controllerConfig.getFirstIteration(), this.controllerConfig.getLastIteration() + 2);
+		ValueAxis yAxis = axisBuilder.createValueAxis("Travel Time");
+		//    yAxis.setRange(-0.05, 0.3);
+		//    xAxis.setVisible(false);
+		//    xAxis.setFixedAutoRange(1.0);
+		plot.setDomainAxis(xAxis);
+		plot.setRangeAxis(yAxis);
 
-  public void setStartTime(double sec) {
-    this.startTime  = sec;
-  }
+		DgColorScheme colorScheme = new DgColorScheme();
 
-  public void setEndTime(double sec) {
-    this.endTime = sec;
-  }
+		XYItemRenderer renderer2;
+		renderer2 = new XYLineAndShapeRenderer(true, false);
+		renderer2.setSeriesItemLabelsVisible(0, true);
+		//    renderer2.setSeriesItemLabelGenerator(0, this.labelGenerator);
+		plot.setDataset(0, dataset);
+		renderer2.setSeriesStroke(0, new BasicStroke(1.0f));
+		renderer2.setSeriesOutlineStroke(0, new BasicStroke(1.0f));
+		renderer2.setSeriesPaint(0, colorScheme.getColor(1, "a"));
+		renderer2.setSeriesStroke(1, new BasicStroke(1.0f));
+		renderer2.setSeriesOutlineStroke(1, new BasicStroke(1.0f));
+		renderer2.setSeriesPaint(1, colorScheme.getColor(2, "a"));
+		renderer2.setSeriesStroke(2, new BasicStroke(1.0f));
+		renderer2.setSeriesOutlineStroke(2, new BasicStroke(1.0f));
+		renderer2.setSeriesPaint(2, colorScheme.getColor(3, "a"));
+		renderer2.setSeriesStroke(3, new BasicStroke(1.0f));
+		renderer2.setSeriesOutlineStroke(3, new BasicStroke(1.0f));
+		renderer2.setSeriesPaint(3, colorScheme.getColor(4, "a"));
 
-  
-  
+		plot.setRenderer(0, renderer2);
+
+		JFreeChart chart = new JFreeChart("", plot);
+		chart.setBackgroundPaint(ChartColor.WHITE);
+		chart.getLegend().setItemFont(axisBuilder.getAxisFont());
+		chart.setTextAntiAlias(true);
+		//    chart.removeLegend();
+		return chart;
+	}
+
+	public void setStartTime(double sec) {
+		this.startTime  = sec;
+	}
+
+	public void setEndTime(double sec) {
+		this.endTime = sec;
+	}
+
+
+
 }
