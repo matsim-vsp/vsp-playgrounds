22ca20d046e7a35f23a59096084abc45ff390995
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2019-02-21 14:49:15 +0100

    analysis code

--- stockholm/src/main/java/gunnar/wum/StuckInPTAnalyzer.java
+++ stockholm/src/main/java/gunnar/wum/StuckInPTAnalyzer.java
@@ -19,26 +19,22 @@
  */
 package gunnar.wum;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.PersonStuckEvent;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonStuckEventHandler;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
-import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.api.experimental.events.BoardingDeniedEvent;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.api.experimental.events.handler.BoardingDeniedEventHandler;
-import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
-import org.matsim.core.scenario.ScenarioUtils;
-
-import floetteroed.utilities.FractionalIterable;
 
 /**
  *
@@ -51,7 +47,11 @@ public class StuckInPTAnalyzer
 	final String eventsFileName;
 
 	final Set<Id<Person>> denied = new LinkedHashSet<>();
-	final Set<Id<Person>> stuck = new LinkedHashSet<>();
+	final Set<Id<Person>> stuckPersons = new LinkedHashSet<>();
+	final Set<Id<Link>> stuckLinks = new LinkedHashSet<>();
+	final Map<String, Integer> stuckModes2Cnt = new LinkedHashMap<>();
+	final Map<Id<Link>, Integer> stuckBusPassengerLoc2Cnt = new LinkedHashMap<>();
+	final Map<Id<Link>, Integer> stuckRailPassengerLoc2Cnt = new LinkedHashMap<>();
 	final Set<String> legModes = new LinkedHashSet<>();
 
 	StuckInPTAnalyzer(final String eventsFileName) {
@@ -65,7 +65,16 @@ public class StuckInPTAnalyzer
 
 	@Override
 	public void handleEvent(PersonStuckEvent event) {
-		this.stuck.add(event.getPersonId());
+		this.stuckPersons.add(event.getPersonId());
+		this.stuckLinks.add(event.getLinkId());
+		this.stuckModes2Cnt.put(event.getLegMode(), this.stuckModes2Cnt.getOrDefault(event.getLegMode(), 0) + 1);
+		if ("railPassenger".equals(event.getLegMode())) {
+			this.stuckRailPassengerLoc2Cnt.put(event.getLinkId(),
+					this.stuckRailPassengerLoc2Cnt.getOrDefault(event.getLinkId(), 0) + 1);
+		} else if ("busPassenger".equals(event.getLegMode())) {
+			this.stuckBusPassengerLoc2Cnt.put(event.getLinkId(),
+					this.stuckBusPassengerLoc2Cnt.getOrDefault(event.getLinkId(), 0) + 1);
+		}
 	}
 
 	@Override
@@ -75,7 +84,7 @@ public class StuckInPTAnalyzer
 
 	Set<Id<Person>> getDeniedAndStuckIDs() {
 		Set<Id<Person>> result = new LinkedHashSet<>(this.denied);
-		result.retainAll(this.stuck);
+		result.retainAll(this.stuckPersons);
 		return result;
 	}
 
@@ -95,29 +104,32 @@ public class StuckInPTAnalyzer
 		StuckInPTAnalyzer analyzer = new StuckInPTAnalyzer(eventsFileName);
 		analyzer.run();
 
-		System.out.println("denied:           " + analyzer.denied.size());
-		System.out.println("stuck:            " + analyzer.stuck.size());
-		System.out.println("denied and stuck: " + analyzer.getDeniedAndStuckIDs().size());
-		System.out.println("all leg modes:    " + analyzer.legModes);
+		System.out.println("denied:              " + analyzer.denied.size());
+		System.out.println("stuck:               " + analyzer.stuckPersons.size());
+		System.out.println("denied and stuck:    " + analyzer.getDeniedAndStuckIDs().size());
+		System.out.println("all leg modes:       " + analyzer.legModes);
+		System.out.println("stuck links:         " + analyzer.stuckLinks);
+		System.out.println("stuck links:         " + analyzer.stuckLinks.size());
+		System.out.println("stuck modes:         " + analyzer.stuckModes2Cnt);
+		System.out.println("stuck bus loc2cnt:   " + analyzer.stuckBusPassengerLoc2Cnt);
+		System.out.println("stuck train loc2cnt: " + analyzer.stuckRailPassengerLoc2Cnt);
 
-		Config config = ConfigUtils.createConfig();
-		config.plans().setInputFile(inputPlansFile);
-		Scenario scenario = ScenarioUtils.loadScenario(config);
+		// Config config = ConfigUtils.createConfig();
+		// config.plans().setInputFile(inputPlansFile);
+		// Scenario scenario = ScenarioUtils.loadScenario(config);
 
-		Set<Id<Person>> stuckSample = new LinkedHashSet<>();
-		for (Id<Person> id : new FractionalIterable<>(analyzer.stuck, 0.01)) {
-			stuckSample.add(id);
-		}
-		
-		Set<Id<Person>> nonStuckIds = new LinkedHashSet<>(scenario.getPopulation().getPersons().keySet());
-		nonStuckIds.removeAll(stuckSample);
-		for (Id<Person> removeId : nonStuckIds) {
-			scenario.getPopulation().getPersons().remove(removeId);
-		}
-
-		PopulationWriter writer = new PopulationWriter(scenario.getPopulation());
-		writer.write(outputPlansFile);
-
-		System.out.println("... DONE");
+		// Set<Id<Person>> stuckSample = new LinkedHashSet<>();
+		// for (Id<Person> id : new FractionalIterable<>(analyzer.stuck, 0.01)) {
+		// stuckSample.add(id);
+		// }
+		// Set<Id<Person>> nonStuckIds = new
+		// LinkedHashSet<>(scenario.getPopulation().getPersons().keySet());
+		// nonStuckIds.removeAll(stuckSample);
+		// for (Id<Person> removeId : nonStuckIds) {
+		// scenario.getPopulation().getPersons().remove(removeId);
+		// }
+		// PopulationWriter writer = new PopulationWriter(scenario.getPopulation());
+		// writer.write(outputPlansFile);
+		// System.out.println("... DONE");
 	}
 }
