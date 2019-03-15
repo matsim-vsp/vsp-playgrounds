f74fb5d20c8a809833f4db9b42c258bc75d5dce2
Michal Maciejewski <michal.mac@gmail.com>
2019-03-15 17:13:42 +0100

    add PassengerRequestScheduled/SubmittedEvent + handlers; keep DrtRequestSubmittedEvent as a subclass

--- gleich/src/main/java/playground/gleich/av_bus/analysis/drtVehicles/DrtVehiclePositionWriter.java
+++ gleich/src/main/java/playground/gleich/av_bus/analysis/drtVehicles/DrtVehiclePositionWriter.java
@@ -37,11 +37,11 @@ import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonLeavesVehicleEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.contrib.drt.passenger.events.DrtRequestScheduledEvent;
-import org.matsim.contrib.drt.passenger.events.DrtRequestScheduledEventHandler;
 import org.matsim.contrib.dvrp.fleet.DvrpVehicleSpecification;
 import org.matsim.contrib.dvrp.fleet.FleetReader;
 import org.matsim.contrib.dvrp.fleet.FleetSpecificationImpl;
+import org.matsim.contrib.dvrp.passenger.PassengerRequestScheduledEvent;
+import org.matsim.contrib.dvrp.passenger.PassengerRequestScheduledEventHandler;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
@@ -52,60 +52,57 @@ import org.matsim.vehicles.Vehicle;
 
 import playground.gleich.analysis.experiencedTrips.DrtEventsReader;
 
-
 /**
- * 
  * @author gleich
- * 
  */
-public class DrtVehiclePositionWriter implements DrtRequestScheduledEventHandler, 
-LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEventHandler {
-	
+public class DrtVehiclePositionWriter
+		implements PassengerRequestScheduledEventHandler, LinkEnterEventHandler, PersonEntersVehicleEventHandler,
+		PersonLeavesVehicleEventHandler {
+
 	private static String NetworkFile = "/Users/gleich/Documents/EclipseWorkspace/av-bus_berlinNW/data/output/Berlin100pct/D2D/D2D_250_Cap4/output_network.xml.gz";
 	private static String EventsFile = "/Users/gleich/Documents/EclipseWorkspace/av-bus_berlinNW/data/output/Berlin100pct/D2D/old_D2D_400_Cap4/output_events.xml.gz";
 	private static String drtVehicleFile = "/Users/gleich/Documents/EclipseWorkspace/av-bus_berlinNW/data/input/Berlin100pct/drt/DrtVehicles.100pct.DRT_400_Cap4.xml";
 	private static String outputDirectoryString = "/Users/gleich/Documents/EclipseWorkspace/av-bus_berlinNW/data/output/Berlin100pct/D2D/old_D2D_400_Cap4/drtVehiclePositions";
-	
+
 	private Network network;
-	
-	/** 
+
+	/**
 	 * current implementation counts scheduled requests and substracts alighted passengers
 	 * -> won't work if one scheduled request contains multiple passengers, which currently is not possible
-	 * 
+	 * <p>
 	 * scheduleCounter is broken, delivers negative counts
 	 */
 	private Map<Id<Vehicle>, Integer> drtVeh2scheduleCounter = new HashMap<>();
 	private Map<Id<Vehicle>, Integer> drtVeh2CurrentNumPassengers = new HashMap<>();
 	private Map<Id<Vehicle>, Id<Link>> drtVeh2CurrentLink = new HashMap<>();
-	
+
 	private String outputDirectory;
 	private double nextOutputTime;
 	private final double outputInterval;
 	private String sep = ";";
-	
+
 	public static void main(String[] args) throws MalformedURLException {
 		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 		new MatsimNetworkReader(scenario.getNetwork()).readFile(NetworkFile);
 		EventsManager events = EventsUtils.createEventsManager();
-		DrtVehiclePositionWriter eventHandler = new DrtVehiclePositionWriter(scenario.getNetwork(), new URL("file://" + drtVehicleFile), 
-				outputDirectoryString, 15*60);
+		DrtVehiclePositionWriter eventHandler = new DrtVehiclePositionWriter(scenario.getNetwork(),
+				new URL("file://" + drtVehicleFile), outputDirectoryString, 15 * 60);
 		events.addHandler(eventHandler);
 		new DrtEventsReader(events).readFile(EventsFile);
 	}
-	
+
 	/**
-	 * 
 	 * @param network
 	 */
-	public DrtVehiclePositionWriter(Network network, URL drtVehiclesFileUrl, String outputDirectory, 
-			double outputInterval){
+	public DrtVehiclePositionWriter(Network network, URL drtVehiclesFileUrl, String outputDirectory,
+			double outputInterval) {
 		this.network = network;
 		this.outputDirectory = outputDirectory;
 		this.outputInterval = outputInterval;
 		this.nextOutputTime = outputInterval;
 		initializeFromVehiclesFile(drtVehiclesFileUrl);
 	}
-	
+
 	private void initializeFromVehiclesFile(URL drtVehiclesFileUrl) {
 		FleetSpecificationImpl fleet = new FleetSpecificationImpl();
 		new FleetReader(fleet).parse(drtVehiclesFileUrl);
@@ -115,11 +112,11 @@ LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEvent
 			drtVeh2scheduleCounter.put(Id.createVehicleId(s.getId()), 0);
 		}
 	}
-	
+
 	private void calcNextOutputTime() {
 		nextOutputTime += outputInterval;
 	}
-	
+
 	private void outputWriterTrigger(double eventTime) {
 		if (eventTime >= nextOutputTime) {
 			writeOutput(nextOutputTime);
@@ -130,7 +127,7 @@ LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEvent
 	@Override
 	public void reset(int iteration) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	// fetch vehicle movements
@@ -142,13 +139,12 @@ LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEvent
 		}
 	}
 
-
 	// detect boarding passengers
 	@Override
 	public void handleEvent(PersonEntersVehicleEvent event) {
 		outputWriterTrigger(event.getTime());
-		if (drtVeh2CurrentNumPassengers.containsKey(event.getVehicleId())) {	
-			drtVeh2CurrentNumPassengers.put(event.getVehicleId(), 
+		if (drtVeh2CurrentNumPassengers.containsKey(event.getVehicleId())) {
+			drtVeh2CurrentNumPassengers.put(event.getVehicleId(),
 					drtVeh2CurrentNumPassengers.get(event.getVehicleId()) + 1);
 		}
 	}
@@ -157,39 +153,45 @@ LinkEnterEventHandler, PersonEntersVehicleEventHandler, PersonLeavesVehicleEvent
 	@Override
 	public void handleEvent(PersonLeavesVehicleEvent event) {
 		outputWriterTrigger(event.getTime());
-		if (drtVeh2CurrentNumPassengers.containsKey(event.getVehicleId())) {	
-			drtVeh2CurrentNumPassengers.put(event.getVehicleId(), 
+		if (drtVeh2CurrentNumPassengers.containsKey(event.getVehicleId())) {
+			drtVeh2CurrentNumPassengers.put(event.getVehicleId(),
 					drtVeh2CurrentNumPassengers.get(event.getVehicleId()) - 1);
-			drtVeh2scheduleCounter.put(event.getVehicleId(), 
-					drtVeh2scheduleCounter.get(event.getVehicleId()) - 1);
+			drtVeh2scheduleCounter.put(event.getVehicleId(), drtVeh2scheduleCounter.get(event.getVehicleId()) - 1);
 		}
 	}
 
 	// detect new scheduled requests
 	@Override
-	public void handleEvent(DrtRequestScheduledEvent event) {
+	public void handleEvent(PassengerRequestScheduledEvent event) {
 		outputWriterTrigger(event.getTime());
-		drtVeh2scheduleCounter.put(Id.createVehicleId(event.getVehicleId()), 
+		drtVeh2scheduleCounter.put(Id.createVehicleId(event.getVehicleId()),
 				drtVeh2scheduleCounter.get(event.getVehicleId()) + 1);
 	}
-	
+
 	private void writeOutput(double outputTime) {
 		BufferedWriter bw;
 		try {
 			bw = IOUtils.getBufferedWriter(outputDirectory + "/drtVehPositions_" + (int)outputTime + ".csv");
 			// write header
-			bw.write("vehId" + sep + "linkId" + sep + "x" + sep + "y" + sep +
-					"passengers" + sep + "scheduledRequests");
+			bw.write("vehId" + sep + "linkId" + sep + "x" + sep + "y" + sep + "passengers" + sep + "scheduledRequests");
 			bw.newLine();
-			
+
 			for (Id<Vehicle> id : drtVeh2CurrentLink.keySet()) {
 				Coord linkMidPoint = network.getLinks().get(drtVeh2CurrentLink.get(id)).getCoord();
-				bw.write(id.toString() + sep + drtVeh2CurrentLink.get(id).toString() + sep + 
-						linkMidPoint.getX() + sep + linkMidPoint.getY() + sep +
-						drtVeh2CurrentNumPassengers.get(id) + sep + drtVeh2scheduleCounter.get(id));
+				bw.write(id.toString()
+						+ sep
+						+ drtVeh2CurrentLink.get(id).toString()
+						+ sep
+						+ linkMidPoint.getX()
+						+ sep
+						+ linkMidPoint.getY()
+						+ sep
+						+ drtVeh2CurrentNumPassengers.get(id)
+						+ sep
+						+ drtVeh2scheduleCounter.get(id));
 				bw.newLine();
 			}
-			
+
 			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
