3f04994127a515ef22a01b9cbb4aa1b7ae8a29a3
GunnarF <GunnarF@GUNNARF-830G5.vti.se>
2019-05-19 09:26:48 +0200

    minor

--- stockholm/src/main/java/gunnar/ihop4/rvu2013/Tour.java
+++ stockholm/src/main/java/gunnar/ihop4/rvu2013/Tour.java
@@ -24,7 +24,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.matsim.contrib.greedo.utils.SetUtils;
+import org.matsim.contrib.greedo_DEPRECATED.utils.SetUtils;
 import org.matsim.core.utils.collections.Tuple;
 
 /**
