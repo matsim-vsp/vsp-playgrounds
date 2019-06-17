5e03fbca095cba62810c554086e8297bcced23eb
Marc <dziakowski@vsp.tu-berlin.de>
2019-06-17 14:00:01 +0200

    crs fix

--- vspshk/src/main/java/playground/mdziakowski/activityReLocation/RunOSMBerlinActivities.java
+++ vspshk/src/main/java/playground/mdziakowski/activityReLocation/RunOSMBerlinActivities.java
@@ -15,33 +15,33 @@ public class RunOSMBerlinActivities {
     	
 
 //        String root = "D:/Arbeit/Berlin/ReLocation/Test/";
-    	 String root = "D:/Arbeit/Berlin/ReLocation/exception/";
+//    	 String root = "D:/Arbeit/Berlin/ReLocation/exception/";
 
-                String inputOSMFile = root + "bremen-latest.osm_01.osm";
-//        String inputOSMFile = args[0];
-        String outputFacilityFile = root + "facilities.xml.gz";
+//                String inputOSMFile = root + "bremen-latest.osm_01.osm";
+        String inputOSMFile = args[0];
+//        String outputFacilityFile = root + "facilities.xml.gz";
 //        String outputFacilityFile = root + "combinedFacilities-test1.xml";
-        String attributeFile = root + "attributeFile-test1.xml";
+//        String attributeFile = root + "attributeFile-test1.xml";
         
-//        String outputFacilityFile = "./combinedFacilities-test1.xml";
-//        String attributeFile = "./attributeFile-test1.xml";
+        String outputFacilityFile = "./combinedFacilities-test1.xml";
+        String attributeFile = "./attributeFile-test1.xml";
         
         OutputDirectoryLogging.catchLogEntries();
         try {
-        OutputDirectoryLogging.initLoggingWithOutputDirectory(root + "/log");
+        OutputDirectoryLogging.initLoggingWithOutputDirectory("./log");
         } catch (IOException e1) {
         e1.printStackTrace();
         }
         
-//        String newCoord = args[1];
-        String newCoord = "EPSG:31468";
+        String newCoord = args[1];
+//        String newCoord = "EPSG:31468";
         
         CombinedOsmReader activitiesReader = new CombinedOsmReader(newCoord,
                 AccessibilityFacilityUtils.buildOsmLandUseToMatsimTypeMap(),
                 AccessibilityFacilityUtils.buildOsmBuildingToMatsimTypeMap(),
-                AccessibilityFacilityUtils.buildOsmAmenityToMatsimTypeMap(),
-                AccessibilityFacilityUtils.buildOsmLeisureToMatsimTypeMap(),
-                AccessibilityFacilityUtils.buildOsmTourismToMatsimTypeMap(),
+                AccessibilityFacilityUtils.buildOsmAmenityToMatsimTypeMapV2(),
+                AccessibilityFacilityUtils.buildOsmLeisureToMatsimTypeMapV2(),
+                AccessibilityFacilityUtils.buildOsmTourismToMatsimTypeMapV2(),
                 AccessibilityFacilityUtils.buildUnmannedEntitiesList(),
                 0);
 
