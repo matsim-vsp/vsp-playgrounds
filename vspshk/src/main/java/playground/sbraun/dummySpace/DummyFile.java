c44329f3874ed654209e0134a4e7df6d38265841
soehnkebraun <braun@vsp.tu-berlin.de>
2019-08-22 13:18:27 +0200

    Testcommit after a long time

---
+++ vspshk/src/main/java/playground/sbraun/dummySpace/DummyFile.java
@@ -0,0 +1,12 @@
+package playground.sbraun.dummySpace;
+
+/**
+ * @author sbraun
+ *
+ */
+
+public class DummyFile {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
