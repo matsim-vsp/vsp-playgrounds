ef8a4da76f5ec783778d8ee3bb0f492a70036dd7
kainagel <nagel@vsp.tu-berlin.de>
2019-09-13 18:37:30 +0200

    ... and the corresponding import statements.  Arghh

--- kai/src/main/java/playground/kai/usecases/WrapScoringFunctions.java
+++ kai/src/main/java/playground/kai/usecases/WrapScoringFunctions.java
@@ -3,8 +3,6 @@ package playground.kai.usecases;
 import com.google.inject.Module;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
-import org.matsim.contrib.bicycle.BicycleConfigGroup;
-import org.matsim.contrib.bicycle.BicycleModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.*;
