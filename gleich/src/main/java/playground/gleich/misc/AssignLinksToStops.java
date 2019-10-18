3b1f05a62453d7d2564a7fae1968ac6ebefc2f42
leich <leich@vsp.tu-berlin.de>
2019-10-18 09:15:02 +0200

    use FacilitiesUtils.decideOnLink instead of directly NetworkUtils.getNearestLink

--- gleich/src/main/java/playground/gleich/misc/AssignLinksToStops.java
+++ gleich/src/main/java/playground/gleich/misc/AssignLinksToStops.java
@@ -8,6 +8,7 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.facilities.FacilitiesUtils;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
 import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
@@ -23,7 +24,7 @@ public class AssignLinksToStops {
 	public static void main(String[] args) {
 		String networkFile = "/home/gregor/git/matsim/examples/scenarios/mielec/network.xml";
 		String inputScheduleFile = "/home/gregor/git/matsim/examples/scenarios/mielec/drtstops.xml";
-		String outputScheduleFile = "/home/gregor/git/matsim/examples/scenarios/mielec/drtstops_wLinkIds_lessStops.xml";
+		String outputScheduleFile = "/home/gregor/git/matsim/examples/scenarios/mielec/drtstops_wLinkIds_lessStops2.xml";
 		
 		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 		new TransitScheduleReader(scenario).readFile(inputScheduleFile);
@@ -33,7 +34,7 @@ public class AssignLinksToStops {
 		
 		for (TransitStopFacility stop: scenario.getTransitSchedule().getFacilities().values()) {
 			if (stop.getLinkId() == null) {
-				stop.setLinkId(NetworkUtils.getNearestLink(scenario.getNetwork(), stop.getCoord()).getId());
+				stop.setLinkId(FacilitiesUtils.decideOnLink(stop, scenario.getNetwork()).getId());
 			}
 			
 			if (Math.random() < 0.5) {
