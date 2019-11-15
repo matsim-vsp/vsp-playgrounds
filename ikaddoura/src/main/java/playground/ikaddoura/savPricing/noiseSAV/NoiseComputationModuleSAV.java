010b2ccf0da3dcb494f9feed67fc3f82f7bc188a
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-11-15 23:39:19 +0100

    fix failing test

--- ikaddoura/src/main/java/playground/ikaddoura/savPricing/noiseSAV/NoiseComputationModuleSAV.java
+++ ikaddoura/src/main/java/playground/ikaddoura/savPricing/noiseSAV/NoiseComputationModuleSAV.java
@@ -20,7 +20,7 @@
 package playground.ikaddoura.savPricing.noiseSAV;
 
 import org.apache.log4j.Logger;
-import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.noise.NoiseComputationModule;
 import org.matsim.contrib.noise.NoiseConfigGroup;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
@@ -32,39 +32,19 @@ import org.matsim.core.controler.AbstractModule;
 public class NoiseComputationModuleSAV extends AbstractModule {
 	private static final Logger log = Logger.getLogger(NoiseComputationModuleSAV.class);
 
-	private final Scenario scenario;
-
-	public NoiseComputationModuleSAV(Scenario scenario) {
-		this.scenario = scenario;
-	}
-
 	@Override
 	public void install() {
 		
 		NoiseConfigGroup noiseParameters = ConfigUtils.addOrGetModule(this.getConfig(), NoiseConfigGroup.class);
 
-//		NoiseContext noiseContext = new NoiseContext(this.scenario);
-//		this.bind(NoiseContext.class).toInstance(noiseContext);
-//		
-//		this.bind(NoiseTimeTracker.class).asEagerSingleton();
-//		this.addEventHandlerBinding().to(NoiseTimeTracker.class);
-//				
-//		if (noiseParameters.isUseActualSpeedLevel()) {
-//			this.addEventHandlerBinding().to(LinkSpeedCalculation.class).asEagerSingleton();
-//		}
-//		
-//		if (noiseParameters.isComputePopulationUnits()) {
-//			this.addEventHandlerBinding().toInstance(new PersonActivityTracker(noiseContext));
-//		}
+		this.install(new NoiseComputationModule());
 				
 		if (noiseParameters.isInternalizeNoiseDamages()) {
 			
 			this.addEventHandlerBinding().to(NoisePricingHandlerSAV.class).asEagerSingleton();
 			
 			log.info("Internalizing noise damages. This requires that the default travel disutility is replaced by a travel distuility which accounts for noise tolls.");
-		}
-		
-//		this.addControlerListenerBinding().to(NoiseCalculationOnline.class);
+		}		
 	}
 
 }
