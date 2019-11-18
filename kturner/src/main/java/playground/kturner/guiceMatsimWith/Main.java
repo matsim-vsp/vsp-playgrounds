5bdbe85306d383f16535d019fffd36eba8974168
kturner <martins-turner@vsp.tu-berlin.de>
2019-11-18 15:12:06 +0100

    cleanUp

--- kturner/src/main/java/playground/kturner/guiceMatsimWith/Main.java
+++ kturner/src/main/java/playground/kturner/guiceMatsimWith/Main.java
@@ -30,17 +30,6 @@ import java.util.List;
 public class Main {
 
     public static void main( String[] args) {
-//--- This was without MATSim stuff
-//        Module module = new AbstractModule(){
-//            @Override protected void configure(){
-//                bind (Abc.class).to(AbcImpl1.class);
-//                bind (Helper.class).to(HelperImpl2.class);
-//            }
-//        };
-//        Injector injector = Guice.createInjector(module);
-//        Abc abc = injector.getInstance(Abc.class);
-//        abc.doSomething();
-// ---------
 
         URL url = IOUtils.extendUrl(ExamplesUtils.getTestScenarioURL("equil"), "config.xml");
         Config config = ConfigUtils.loadConfig(url);
@@ -71,40 +60,4 @@ public class Main {
         }
     }
 
-    private static class HelperImpl2 implements Helper{
-        @Override
-        public void doHelp() {
-            System.out.println("calling Helper2");
-        }
-    }
-
-
-    //---above is your "matsim-script-in-java"
-
-    //below are some implementations ion matsim framework
-
-    private interface Abc{
-        void doSomething();
-    }
-
-    private static class AbcImpl1 implements Abc{
-        @Inject Helper helper;
-        @Override
-        public void doSomething() {
-            System.out.println("calling impl1");
-            helper.doHelp();
-        }
-    }
-
-
-    private interface Helper{
-        void doHelp();
-    }
-
-    private static class HelperImpl1 implements Helper{
-        @Override
-        public void doHelp() {
-            System.out.println("calling Helper1");
-        }
-    }
 }
