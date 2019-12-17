4c69b7d83ceaad3f21203cd97cb0533bf2cb41e3
ikaddoura <kaddoura@vsp.tu-berlin.de>
2019-12-17 17:18:01 +0100

    add network2shp script

---
+++ ikaddoura/src/main/java/playground/ikaddoura/analysis/WriteNetwork2ShapeFile.java
@@ -0,0 +1,44 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package playground.ikaddoura.analysis;
+
+import org.matsim.analysis.shapes.Network2Shape;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+
+/**
+* @author ikaddoura
+*/
+
+public class WriteNetwork2ShapeFile {
+
+	public static void main(String[] args) {
+		Config config = ConfigUtils.createConfig();
+		config.network().setInputFile("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-network.xml.gz");
+		config.global().setCoordinateSystem("EPSG:31468");
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Network2Shape.exportNetwork2Shp(scenario, "./scenarios/", "EPSG:31468", TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:31468"));
+	}
+
+}
+
