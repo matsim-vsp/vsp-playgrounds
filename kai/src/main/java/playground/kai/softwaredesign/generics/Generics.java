4545c3702938022493ef8fcec01023104f58c6f1
kainagel <nagel@vsp.tu-berlin.de>
2020-02-07 17:46:33 +0100

    trying around with generics

--- kai/src/main/java/playground/kai/softwaredesign/generics/Generics.java
+++ kai/src/main/java/playground/kai/softwaredesign/generics/Generics.java
@@ -16,7 +16,7 @@
  *   See also COPYING, LICENSE and WARRANTY file                           *
  *                                                                         *
  * *********************************************************************** */
-package playground.kai.test;
+package playground.kai.softwaredesign.generics;
 
 /**
  * @author nagel
