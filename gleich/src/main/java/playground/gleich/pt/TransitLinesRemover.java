209ad6e985445a64238ac9c858809e8780bcb29a
leich <leich@vsp.tu-berlin.de>
2020-09-10 17:22:49 +0200

    commit uncommited changes

---
+++ gleich/src/main/java/playground/gleich/pt/TransitLinesRemover.java
@@ -0,0 +1,43 @@
+package playground.gleich.pt;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt.transitSchedule.api.TransitScheduleWriter;
+import playground.vsp.andreas.utils.pt.TransitLineRemover;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TransitLinesRemover {
+
+	public static void main(String[] args) {
+		final String inScheduleFile = "/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v0/optimizedSchedule.xml.gz";
+		final String inNetworkFile  = "/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v0/optimizedNetwork.xml.gz";
+		final String outScheduleFile = "/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v1/optimizedSchedule_wo-TB247-TB252-TB253-TB254-TB256.xml.gz";
+
+		Config config = ConfigUtils.createConfig();
+		config.global().setCoordinateSystem("EPSG:25832");
+		config.transit().setTransitScheduleFile(inScheduleFile);
+		config.network().setInputFile(inNetworkFile);
+		
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		TransitSchedule inTransitSchedule = scenario.getTransitSchedule();
+		
+		Set<Id<TransitLine>> linesToRemove = new HashSet<>();
+		linesToRemove.add(Id.create("ASTTB247 - 1", TransitLine.class));
+		linesToRemove.add(Id.create("ASTTB256 - 1", TransitLine.class));
+
+		linesToRemove.add(Id.create("ASTTB252 - 1", TransitLine.class));
+		linesToRemove.add(Id.create("ASTTB253 - 1", TransitLine.class));
+		linesToRemove.add(Id.create("ASTTB254 - 1", TransitLine.class));
+
+		TransitSchedule outTransitSchedule = TransitLineRemover.removeTransitLinesFromTransitSchedule(inTransitSchedule, linesToRemove);
+		new TransitScheduleWriter(outTransitSchedule).writeFile(outScheduleFile);
+	}
+
+}
