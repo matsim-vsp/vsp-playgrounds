7d59dccb2bad15b4987374aec976473f60a02c20
leich <leich@vsp.tu-berlin.de>
2020-10-06 11:09:51 +0200

    minor extension

--- gleich/src/main/java/playground/gleich/misc/RunMATSimCodeMadeConfig.java
+++ gleich/src/main/java/playground/gleich/misc/RunMATSimCodeMadeConfig.java
@@ -4,7 +4,9 @@ import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunMATSimCodeMadeConfig {
@@ -13,13 +15,21 @@ public class RunMATSimCodeMadeConfig {
 //		String configFilename = args[0];
 //		Config config = ConfigUtils.loadConfig(configFilename);
 		Config config = ConfigUtils.createConfig();
-		config.network().setInputFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v0/optimizedNetwork.xml.gz");
-		config.transit().setTransitScheduleFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v1/optimizedSchedule_nonSB-bus-split-at-hubs.xml.gz");
-		config.transit().setVehiclesFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v1/optimizedVehicles_nonSB-bus-split-at-hubs.xml.gz");
-		config.transit().setUseTransit(true);
+		config.network().setInputFile("/home/gregor/git/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-network.xml.gz");
+
+//		config.network().setInputFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v0/optimizedNetwork.xml.gz");
+//		config.transit().setTransitScheduleFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v1/optimizedSchedule_nonSB-bus-split-at-hubs.xml.gz");
+//		config.transit().setVehiclesFile("/home/gregor/git/shared-svn/projects/avoev/matsim-input-files/gladbeck_umland/v1/optimizedVehicles_nonSB-bus-split-at-hubs.xml.gz");
+//		config.transit().setUseTransit(true);
+
+		config.plans().setInputFile("/home/gregor/tmp/Vu-DRT-25/testplans.xml");
+
+		config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("home").setTypicalDuration(8*3600));
+		config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("work").setTypicalDuration(5*3600));
 
 		config.controler().setLastIteration(0);
-
+		config.controler().setOutputDirectory("nowhere");
+		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		Controler controler = new Controler(scenario);
