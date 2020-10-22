f7269271b05efd0db43c74172b2677f9ac304a6b
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2020-10-22 13:23:29 +0200

    Adding Zonal matching to the play ground (in progress)

---
+++ lu/src/main/java/run/RunZonalMatching.java
@@ -0,0 +1,52 @@
+package run;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.drt.routing.DrtRoute;
+import org.matsim.contrib.drt.routing.DrtRouteFactory;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtConfigs;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.drt.run.MultiModeDrtModule;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import zoneBasedMatching.ExperimentalModuleExtensiveInserstionWithZC;
+
+public class RunZonalMatching {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			args = new String[] { "C:\\Users\\cluac\\MATSimScenarios\\Mielec\\mielec_drt_config.xml" };
+		}
+		
+		Config config = ConfigUtils.loadConfig(args[0], new MultiModeDrtConfigGroup(), new DvrpConfigGroup());
+		MultiModeDrtConfigGroup multiModeDrtConfig = MultiModeDrtConfigGroup.get(config);
+		DrtConfigs.adjustMultiModeDrtConfig(multiModeDrtConfig, config.planCalcScore(), config.plansCalcRoute());
+		
+		
+		
+		Scenario scenario = ScenarioUtils.createScenario(config);
+		scenario.getPopulation()
+				.getFactory()
+				.getRouteFactories()
+				.setRouteFactory(DrtRoute.class, new DrtRouteFactory());
+		ScenarioUtils.loadScenario(scenario);
+
+		Controler controler = new Controler(scenario);
+		controler.addOverridingModule(new DvrpModule());
+		controler.addOverridingModule(new MultiModeDrtModule());
+		controler.configureQSimComponents(DvrpQSimComponents.activateAllModes(multiModeDrtConfig));
+		
+		// Adding in experimental module manually
+		for (DrtConfigGroup drtCfg : multiModeDrtConfig.getModalElements()) {
+			controler.addOverridingQSimModule(new ExperimentalModuleExtensiveInserstionWithZC(drtCfg));
+		}
+		
+		controler.run();
+		
+	}
+}
---
+++ lu/src/main/java/zoneBasedMatching/ExperimentalModuleExtensiveInserstionWithZC.java
@@ -0,0 +1,59 @@
+package zoneBasedMatching;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.drt.analysis.zonal.DrtZonalSystem;
+import org.matsim.contrib.drt.optimizer.QSimScopeForkJoinPoolHolder;
+import org.matsim.contrib.drt.optimizer.insertion.DetourPathCalculator;
+import org.matsim.contrib.drt.optimizer.insertion.DrtInsertionSearch;
+import org.matsim.contrib.drt.optimizer.insertion.InsertionCostCalculator;
+import org.matsim.contrib.drt.optimizer.insertion.MultiInsertionDetourPathCalculator;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.dvrp.path.OneToManyPathSearch;
+import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
+import org.matsim.contrib.dvrp.run.ModalProviders;
+import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
+import org.matsim.core.mobsim.framework.MobsimTimer;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+
+import com.google.inject.Inject;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Named;
+
+public class ExperimentalModuleExtensiveInserstionWithZC extends AbstractDvrpModeQSimModule {
+
+	private final DrtConfigGroup drtCfg;
+
+	public ExperimentalModuleExtensiveInserstionWithZC(DrtConfigGroup drtCfg) {
+		super(drtCfg.getMode());
+		this.drtCfg = drtCfg;
+	}
+
+	@Override
+	protected void configureQSim() {
+		bindModal(new TypeLiteral<DrtInsertionSearch<OneToManyPathSearch.PathData>>() {
+		}).toProvider(modalProvider(getter -> new ExtensiveInsertionSerachWithZonalConstraints(
+				getter.getModal(DetourPathCalculator.class), drtCfg, getter.get(MobsimTimer.class),
+				getter.getModal(QSimScopeForkJoinPoolHolder.class).getPool(),
+				getter.getModal(InsertionCostCalculator.PenaltyCalculator.class),
+				getter.getModal(DrtZonalSystem.class))));
+
+		addModalComponent(MultiInsertionDetourPathCalculator.class, new ModalProviders.AbstractProvider<>(getMode()) {
+			@Inject
+			@Named(DvrpTravelTimeModule.DVRP_ESTIMATED)
+			private TravelTime travelTime;
+
+			@Override
+			public MultiInsertionDetourPathCalculator get() {
+				Network network = getModalInstance(Network.class);
+				TravelDisutility travelDisutility = getModalInstance(TravelDisutilityFactory.class)
+						.createTravelDisutility(travelTime);
+				return new MultiInsertionDetourPathCalculator(network, travelTime, travelDisutility, drtCfg);
+			}
+		});
+		bindModal(DetourPathCalculator.class).to(modalKey(MultiInsertionDetourPathCalculator.class));
+
+	}
+
+}
